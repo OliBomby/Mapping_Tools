c46899731197fb8adbe961ba92fe31b137e0ed0f
Potoofu <46639313+Potoofu@users.noreply.github.com>
2019-04-06 17:48:29 +0200

    more project stuff

--- Mapping Tools/Classes/SystemTools/HitsoundMaker/Project.cs
+++ Mapping Tools/Classes/SystemTools/HitsoundMaker/Project.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ using System.Threading.Tasks;
 namespace Mapping_Tools.Classes.SystemTools.HitsoundMaker {
     public class Project {
         public String ProjectPath;
+        public String SamplePath;
+        public String ExportPath;
+        public String JSONPath;
         public string BaseBeatmap { get; set; }
         public Sample DefaultSample { get; set; }
         public ObservableCollection<HitsoundLayer> HitsoundLayers { get; set; }
@@ -17,7 +21,14 @@ namespace Mapping_Tools.Classes.SystemTools.HitsoundMaker {
             BaseBeatmap = "";
             DefaultSample = new Sample(0, 0, "", int.MaxValue);
             HitsoundLayers = new ObservableCollection<HitsoundLayer>();
+            SetDefaultPaths(projectpath);
+        }
+
+        private void SetDefaultPaths(String projectpath) {
             ProjectPath = projectpath;
+            SamplePath = Path.Combine(ProjectPath, "Samples");
+            ExportPath = Path.Combine(ProjectPath, "Exports");
+            JSONPath = Path.Combine(ProjectPath, "config.json");
         }
 
         public Project(string baseBeatmap, Sample defaultSample, ObservableCollection<HitsoundLayer> hitsoundLayers, String projectpath) {
@@ -25,6 +36,7 @@ namespace Mapping_Tools.Classes.SystemTools.HitsoundMaker {
             DefaultSample = defaultSample;
             HitsoundLayers = hitsoundLayers;
             ProjectPath = projectpath;
+            SetDefaultPaths(projectpath);
         }
     }
 }
