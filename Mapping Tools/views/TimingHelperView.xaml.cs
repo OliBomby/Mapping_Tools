23735499c8f829b5c390988eb9adeb5d84d7628c
OliBomby <olivierschipper@gmail.com>
2019-04-19 22:50:46 +0200

    Update TimingHelperView.xaml.cs

--- Mapping Tools/views/TimingHelperView.xaml.cs
+++ Mapping Tools/views/TimingHelperView.xaml.cs
@@ -57,7 +57,8 @@ namespace Mapping_Tools.Views {
                 MessageBox.Show(ex.Message);
                 return;
             }
-            backgroundWorker.RunWorkerAsync(new Arguments(fileToCopy, (bool)BookmarkBox.IsChecked, (bool)GreenlinesBox.IsChecked, (bool)OmitBarlineBox.IsChecked));
+            backgroundWorker.RunWorkerAsync(new Arguments(fileToCopy, (bool)BookmarkBox.IsChecked, (bool)GreenlinesBox.IsChecked, (bool)OmitBarlineBox.IsChecked,
+                                                          int.Parse(Snap1.Text.Split('/')[1]), int.Parse(Snap2.Text.Split('/')[1])));
             start.IsEnabled = false;
         }
 
@@ -66,21 +67,57 @@ namespace Mapping_Tools.Views {
             public bool Bookmarks;
             public bool Greenlines;
             public bool OmitBarline;
-            public Arguments(string path, bool bookmarks, bool greenlines, bool omitBarline)
+            public int Snap1;
+            public int Snap2;
+            public Arguments(string path, bool bookmarks, bool greenlines, bool omitBarline, int snap1, int snap2)
             {
                 Path = path;
                 Bookmarks = bookmarks;
                 Greenlines = greenlines;
                 OmitBarline = omitBarline;
+                Snap1 = snap1;
+                Snap2 = snap2;
             }
         }
 
         private string Copy_Hitsounds(Arguments arg, BackgroundWorker worker, DoWorkEventArgs e) {
-            Editor editorTo = new Editor(arg.Path);
-            Beatmap beatmapTo = editorTo.Beatmap;
+            // Open beatmap
+            Editor editor = new Editor(arg.Path);
+            Beatmap beatmap = editor.Beatmap;
+            Timing timing = beatmap.BeatmapTiming;
+
+            // Get all the times to snap
+            List<double> times = new List<double>();
+            if (arg.Bookmarks) {
+                times.AddRange(beatmap.GetBookmarks());
+            }
+            if (arg.Greenlines) {
+                // Get the offsets of greenlines
+                times.AddRange(beatmap.BeatmapTiming.TimingPoints.Where(o => o.Inherited == false).Select(o => o.Offset));
+            }
+
+            // Loop through all the times
+            for (int i = 0; i < times.Count; i++) {
+                double time = times[i];
+                double resnappedTime = timing.Resnap(time, arg.Snap1, arg.Snap2, false);
+
+                TimingPoint redline = timing.GetRedlineAtTime(resnappedTime);
+                double beatsFromRedline = (resnappedTime - redline.Offset) / redline.MpB;
+
+                // Get the times between redline and this time
+                List<double> timesBefore = times.Where(o => o < time && o > redline.Offset).ToList();
+
+                // For each their beatsFromRedline must stay the same AND their time must be within 3-6 ms of their resnapped time
+                // If any of these times becomes incompatible, place a new anchor on the last time and not change the previous redline
+
+                // Update progressbar
+                if (worker != null && worker.WorkerReportsProgress) {
+                    worker.ReportProgress(i * 100 / times.Count);
+                }
+            }
             
             // Save the file
-            editorTo.SaveFile();
+            editor.SaveFile();
 
             // Complete progressbar
             if (worker != null && worker.WorkerReportsProgress) {
