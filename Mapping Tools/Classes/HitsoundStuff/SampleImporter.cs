f79c044c79922b4ffa90b56aea25c805a44ee06b
OliBomby <olivierschipper@gmail.com>
2019-06-11 01:33:58 +0200

    Super Scuffed soundfont playing

--- Mapping Tools/Classes/HitsoundStuff/SampleImporter.cs
+++ Mapping Tools/Classes/HitsoundStuff/SampleImporter.cs
@@ -1,4 +1,5 @@
-﻿using NAudio.Vorbis;
+﻿using NAudio.SoundFont;
+using NAudio.Vorbis;
 using NAudio.Wave;
 using System;
 using System.Collections.Generic;
@@ -37,9 +38,60 @@ namespace Mapping_Tools.Classes.HitsoundStuff {
         }
 
         public static WaveStream ImportSample(string path) {
-            string p = path.Split('?')[0];
+            string[] split = path.Split('?');
+            string p = split[0];
             if (Path.GetExtension(p) == ".sf2") {
-                // do stuff
+                int[] args = split[1].Split('\\').Select(o => int.Parse(o)).ToArray();
+                
+                SoundFont soundFont = new SoundFont(p);
+                Console.WriteLine(soundFont.FileInfo.ToString());
+
+                foreach (Zone z in soundFont.Instruments[4].Zones) {
+                    Console.WriteLine("Zone:");
+                    foreach (Generator g in z.Generators) {
+                        Console.WriteLine(g.ToString());
+                        Console.WriteLine(g.SampleHeader);
+
+                        if (g.SampleHeader != null) {
+                            SampleHeader s = g.SampleHeader;
+
+                            if (s.OriginalPitch == args[1]) {
+                                Console.WriteLine("yeet found it");
+                                int length = (int)(s.End - s.Start);
+                                Console.WriteLine("length: " + length);
+                                byte[] buffer = new byte[length];
+                                Array.Copy(soundFont.SampleData, s.Start, buffer, 0, length);
+                                return new RawSourceWaveStream(buffer, 0, length, new WaveFormat((int)s.SampleRate, 2));
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine("Instruments:");
+                foreach (Instrument i in soundFont.Instruments) {
+                    Console.WriteLine(i.Name);
+                    foreach (Zone z in i.Zones) {
+                        Console.WriteLine("Zone:");
+                        foreach (Generator g in z.Generators) {
+                            Console.WriteLine(g.ToString());
+                            Console.WriteLine(g.SampleHeader);
+
+                            if (g.SampleHeader != null) {
+                                SampleHeader s = g.SampleHeader;
+
+                                if (s.OriginalPitch == args[1]) {
+                                    Console.WriteLine("yeet found it");
+                                    int length = (int)(s.End - s.Start);
+                                    Console.WriteLine("length: " + length);
+                                    byte[] buffer = new byte[length];
+                                    Array.Copy(soundFont.SampleData, s.Start, buffer, 0, length);
+                                    return new RawSourceWaveStream(buffer, 0, length, new WaveFormat((int)s.SampleRate, 2));
+                                }
+                            }
+                        }
+                    }
+                }
+
                 return null;
             } else if (Path.GetExtension(p) == ".ogg") {
                 return new VorbisWaveReader(p);
