66b92f0bcd38d038c52285160f2b9e0185681bb1
OliBomby <olivierschipper@gmail.com>
2019-06-19 16:54:57 +0200

    updated validation to accept empty string

--- Mapping Tools/Components/Domain/ParsableDoubleListValidationRule.cs
+++ Mapping Tools/Components/Domain/ParsableDoubleListValidationRule.cs
@@ -7,7 +7,7 @@ namespace Mapping_Tools.Components.Domain
     class ParsableDoubleListValidationRule : ValidationRule
     {
         public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
-            if (Regex.IsMatch((value ?? "").ToString(), @"^[0-9]+(\.[0-9]+)?(,[0-9]+(\.[0-9]+)?)*$")) {
+            if (Regex.IsMatch((value ?? "").ToString(), @"^([0-9]+(\.[0-9]+)?(,[0-9]+(\.[0-9]+)?)*)?$")) {
                 return ValidationResult.ValidResult;
             } else {
                 return new ValidationResult(false, "Field cannot be parsed.");
