4d9e03805a63a21df787a1e8007cec2bb5f6c091
OliBomby <olivierschipper@gmail.com>
2019-06-24 00:26:48 +0200

    add converter and vm

---
+++ Mapping Tools/Components/Domain/StringToDoubleConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Mapping_Tools.Classes.SystemTools;
+
+namespace Mapping_Tools.Components.Domain
+{
+    class StringToDoubleConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            TypeConverters.TryParseDouble(value.ToString(), out double result, (double)parameter);
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ((double)value).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Mapping Tools/viewmodels/PropertyTransformerVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace Mapping_Tools.Viewmodels {
+    public class PropertyTransformerVM : INotifyPropertyChanged{
+        private double timingpointOffsetMultiplier;
+        public double TimingpointOffsetMultiplier {
+            get { return timingpointOffsetMultiplier; }
+            set {
+                if (timingpointOffsetMultiplier != value) {
+                    timingpointOffsetMultiplier = value;
+                    NotifyPropertyChanged("TimingpointOffsetMultiplier");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public PropertyTransformerVM() {
+            TimingpointOffsetMultiplier = 134;
+        }
+    }
+}
