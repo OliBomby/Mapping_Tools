a4dda99432ae36bb58d808a67bde9bcfcfa23a09
OliBomby <olivierschipper@gmail.com>
2019-06-30 22:31:42 +0200

    Create StoryBoardSample.cs

---
+++ Mapping Tools/Classes/BeatmapHelper/StoryBoardSample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping_Tools.Classes.BeatmapHelper {
+    public class StoryboardSample {
+        public enum Layers {
+            Background = 0,
+            Fail = 1,
+            Pass = 2,
+            Foreground = 3
+        }
+
+
+        public double Time { get; set; }
+        public int Layer { get; set; }
+        public string FilePath { get; set; }
+        public double Volume { get; set; }
+
+        // Sample,56056,0,"soft-hitnormal.wav",30
+        public StoryboardSample(double time, int layer, string filePath, double volume) {
+            Time = time;
+            Layer = layer;
+            FilePath = filePath;
+            Volume = volume;
+        }
+
+        public StoryboardSample(string line) {
+            string[] values = line.Split(',');
+
+            if (values[0] != "Sample") {
+                throw new Exception("Can not parse this line because it's not a storyboarded sample.");
+            }
+
+            Time = ParseDouble(values[1]);
+            Layer = int.Parse(values[2]);
+            FilePath = values[3].Trim('"');
+            Volume = values.Length >= 5 ? ParseDouble(values[4]) : 100;
+        }
+
+        private double ParseDouble(string d) {
+            return double.Parse(d, CultureInfo.InvariantCulture);
+        }
+
+        public string GetLine() {
+            return String.Format("Sample,{0},{1},\"{2}\",{3}", Math.Round(Time), Layer, FilePath, Math.Round(Volume));
+        }
+    }
+}
