3ca840d819dfd425c49ad6c9ec5025d048f1f076
OliBomby <olivierschipper@gmail.com>
2019-07-20 15:49:07 +0200

    remove ':' from error message

--- Mapping Tools/views/MapCleaner/CleanerView.xaml.cs
+++ Mapping Tools/views/MapCleaner/CleanerView.xaml.cs
@@ -51,7 +51,7 @@ namespace Mapping_Tools.Views {
 
         private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
             if( e.Error != null ) {
-                MessageBox.Show(string.Format("{0}:{1}{2}", e.Error.Message, Environment.NewLine, e.Error.StackTrace), "Error");
+                MessageBox.Show(string.Format("{0}{1}{2}", e.Error.Message, Environment.NewLine, e.Error.StackTrace), "Error");
             }
             else {
                 FillTimeLine();
