4ad8a187bcb12e5218627822a01783aa5044c56c
OliBomby <olivierschipper@gmail.com>
2019-08-10 11:50:05 +0200

    updated sample.cs and bindings

--- Mapping Tools/Classes/HitsoundStuff/Sample.cs
+++ Mapping Tools/Classes/HitsoundStuff/Sample.cs
@@ -1,39 +1,75 @@
-﻿namespace Mapping_Tools.Classes.HitsoundStuff {
-    public class Sample {
-        public SampleSet SampleSet { get; set; }
-        public Hitsound Hitsound { get; set; }
-        public SampleGeneratingArgs SampleArgs { get; set; }
-        public int Priority { get; set; }
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-        public int SampleSetComboBoxIndex { get => GetSampleSetComboBoxIndex(); set => SetSampleSetComboBoxIndex(value); }
+namespace Mapping_Tools.Classes.HitsoundStuff {
+    public class Sample : INotifyPropertyChanged {
+        private SampleGeneratingArgs _sampleArgs;
+        private int _priority;
+        private SampleSet _sampleSet;
+        private Hitsound _hitsound;
 
-        private void SetSampleSetComboBoxIndex(int value) {
-            SampleSet = (SampleSet)value + 1;
+        public SampleGeneratingArgs SampleArgs {
+            get { return _sampleArgs; }
+            set {
+                if (_sampleArgs == value) return;
+                _sampleArgs = value;
+                OnPropertyChanged();
+            }
         }
 
-        private int GetSampleSetComboBoxIndex() {
-            return (int)SampleSet - 1;
+        public int Priority {
+            get { return _priority; }
+            set {
+                if (_priority == value) return;
+                _priority = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SampleSet SampleSet {
+            get { return _sampleSet; }
+            set {
+                if (_sampleSet == value) return;
+                _sampleSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Hitsound Hitsound {
+            get { return _hitsound; }
+            set {
+                if (_hitsound == value) return;
+                _hitsound = value;
+                OnPropertyChanged();
+            }
         }
 
         public Sample() {
-            SampleSet = 0;
-            Hitsound = 0;
-            SampleArgs = new SampleGeneratingArgs();
-            Priority = 0;
+            _sampleArgs = new SampleGeneratingArgs();
+            _priority = 0;
+            _sampleSet = SampleSet.Normal;
+            _hitsound = Hitsound.Normal;
         }
 
         public Sample(SampleSet sampleSet, Hitsound hitsound, SampleGeneratingArgs samplePath, int priority) {
-            SampleSet = sampleSet;
-            Hitsound = hitsound;
-            SampleArgs = samplePath;
-            Priority = priority;
+            _sampleArgs = samplePath;
+            _priority = priority;
+            _sampleSet = sampleSet;
+            _hitsound = hitsound;
         }
 
         public Sample(HitsoundLayer hl) {
-            SampleSet = hl.SampleSet;
-            Hitsound = hl.Hitsound;
-            SampleArgs = hl.SampleArgs;
-            Priority = hl.Priority;
+            _sampleArgs = hl.SampleArgs;
+            _priority = hl.Priority;
+            _sampleSet = hl.SampleSet;
+            _hitsound = hl.Hitsound;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
