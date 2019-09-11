54ab3cf7aa3101fcb50513c5de86070188336af5
OliBomby <olivierschipper@gmail.com>
2019-09-11 07:23:25 +0200

    debug overlay border

--- Mapping Tools/views/SnappingTools/SnappingToolsOverlay.cs
+++ Mapping Tools/views/SnappingTools/SnappingToolsOverlay.cs
@@ -4,6 +4,8 @@ using Overlay.NET.Common;
 using Overlay.NET.Wpf;
 using Process.NET.Windows;
 using System;
+using System.Windows;
+using System.Windows.Media;
 using OverlayWindow = Overlay.NET.Wpf.OverlayWindow;
 
 namespace Mapping_Tools.Views.SnappingTools {
@@ -24,7 +26,7 @@ namespace Mapping_Tools.Views.SnappingTools {
             OverlayWindow.BorderBrush = Brushes.Blue;
             OverlayWindow.BorderThickness = new Thickness(3);
 #endif
-            
+
             _tickEngine.Interval = TimeSpan.FromMilliseconds(1000 / 60f);
             _tickEngine.PreTick += OnPreTick;
             _tickEngine.Tick += OnTick;
