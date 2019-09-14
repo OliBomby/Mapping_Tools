cff63b1bcd608d57fdf52a902e91215ee262d345
Olivier Schipper <olivierschipper@gmail.com>
2019-09-14 15:23:54 +0200

    more generators

---
+++ Mapping Tools/Classes/SnappingTools/RelevantObjectGenerators/Generators/AnchorPointGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mapping_Tools.Classes.BeatmapHelper;
+
+namespace Mapping_Tools.Classes.SnappingTools.RelevantObjectGenerators.Generators {
+    class AnchorPointGenerator : RelevantObjectsGenerator, IGenerateRelevantObjectsFromHitObjects {
+        public new string Name => "Anchor of Slider Point Generator";
+        public new string Tooltip => "Generates virtual points on anchors of sliders.";
+        public new GeneratorType GeneratorType => GeneratorType.Generators;
+
+        public List<IRelevantObject> GetRelevantObjects(List<HitObject> objects)
+        {
+            return objects.Where(ho => ho.IsSlider)
+                .SelectMany(ho => ho.GetAllCurvePoints().Select(poi => new RelevantPoint(poi))).Cast<IRelevantObject>()
+                .ToList();
+        }
+    }
+}
---
+++ Mapping Tools/Classes/SnappingTools/RelevantObjectGenerators/Generators/LastAnchorGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mapping_Tools.Classes.BeatmapHelper;
+
+namespace Mapping_Tools.Classes.SnappingTools.RelevantObjectGenerators.Generators {
+    class LastAnchorGenerator : RelevantObjectsGenerator, IGenerateRelevantObjectsFromHitObjects {
+        public new string Name => "Point on Last Anchor of Slider Generator";
+        public new string Tooltip => "Generates virtual points on the last anchors of sliders.";
+        public new GeneratorType GeneratorType => GeneratorType.Generators;
+
+        public List<IRelevantObject> GetRelevantObjects(List<HitObject> objects)
+        {
+            return (from ho in objects where ho.IsSlider select new RelevantPoint(ho.CurvePoints.Last())).Cast<IRelevantObject>().ToList();
+        }
+    }
+}
