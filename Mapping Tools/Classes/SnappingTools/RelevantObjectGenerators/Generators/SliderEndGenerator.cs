e2bcb82daf688a4809b40ff2a221efd7aa73510f
Olivier Schipper <olivierschipper@gmail.com>
2019-09-14 15:27:26 +0200

    slider end generator

---
+++ Mapping Tools/Classes/SnappingTools/RelevantObjectGenerators/Generators/SliderEndGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mapping_Tools.Classes.BeatmapHelper;
+
+namespace Mapping_Tools.Classes.SnappingTools.RelevantObjectGenerators.Generators {
+    class SliderEndGenerator : RelevantObjectsGenerator, IGenerateRelevantObjectsFromHitObjects {
+        public new string Name => "Slider End Generator";
+        public new string Tooltip => "Generates virtual points on the actual ends of sliders.";
+        public new GeneratorType GeneratorType => GeneratorType.Generators;
+
+        public List<IRelevantObject> GetRelevantObjects(List<HitObject> objects)
+        {
+            return (from ho in objects where ho.IsSlider select new RelevantPoint(ho.GetSliderPath().PositionAt(1))).Cast<IRelevantObject>().ToList();
+        }
+    }
+}
