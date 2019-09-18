08348589f4cc3f972d9c6f581bffa84b0fdd2290
OliBomby <olivierschipper@gmail.com>
2019-09-18 19:21:12 +0200

    classes for data

---
+++ Mapping Tools/Classes/SnappingTools/RelevantObjectLayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping_Tools.Classes.SnappingTools {
+    public class RelevantObjectLayer : ObjectLayer<IRelevantObject> {
+        public new List<IRelevantObject> ObjectList {
+            get {
+                var objects = Lines.Concat<IRelevantObject>(Circles).Concat(Points).ToList();
+                return objects;
+            }
+            set {
+                Points = value.OfType<RelevantPoint>().ToList();
+                Lines = value.OfType<RelevantLine>().ToList();
+                Circles = value.OfType<RelevantCircle>().ToList();
+            }
+        }
+
+        private List<RelevantPoint> Points;
+        private List<RelevantLine> Lines;
+        private List<RelevantCircle> Circles;
+    }
+}
