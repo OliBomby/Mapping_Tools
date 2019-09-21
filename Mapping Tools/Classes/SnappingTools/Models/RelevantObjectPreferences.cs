0c7c111441eb6f8968b951a90999804eb89c6da3
CrazyRabbitKGe <crazyrabbitkge@gmail.com>
2019-09-21 17:17:51 +0900

    snapping tools preferences restructure

---
+++ Mapping Tools/Classes/SnappingTools/Models/RelevantObjectPreferences.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using Mapping_Tools.Classes.SystemTools;
+
+namespace Mapping_Tools.Classes.SnappingTools {
+    public class RelevantObjectPreferences : BindableBase {
+        #region private storage
+        private string name;
+        private Color color;
+        private double opacity;
+        private double thickness;
+        private DashStylesEnum dashstyle;
+        private double size;
+
+        private bool hasSizeOption;
+        #endregion
+        public string Name {
+            get => name;
+            set => Set(ref name, value);
+        }
+
+        public Color Color {
+            get => color;
+            set => Set(ref color, value);
+        }
+        public double Opacity {
+            get => opacity;
+            set => Set(ref opacity, value);
+        }
+        public double Thickness {
+            get => thickness;
+            set => Set(ref thickness, value);
+        }
+        public DashStylesEnum Dashstyle {
+            get => dashstyle;
+            set => Set(ref dashstyle, value);
+        }
+
+        public double Size {
+            get => size;
+            set => Set(ref size, value);
+        }
+
+        public bool HasSizeOption {
+            get => hasSizeOption;
+            set => Set(ref hasSizeOption, value);
+        }
+
+        public Pen Pen {
+            get => new Pen() {
+                    Brush = new SolidColorBrush {
+                        Color = Color,
+                        Opacity = Opacity,
+                    },
+                    DashStyle = GetDashStyle(),
+                    Thickness = Thickness,
+            };
+        }
+
+        public static IEnumerable<string> DashStylesEnumerable => Enum.GetNames(typeof(DashStylesEnum));
+
+        private DashStyle GetDashStyle() {
+            switch (Dashstyle) {
+                case DashStylesEnum.Dash:
+                    return DashStyles.Dash;
+                case DashStylesEnum.Dot:
+                    return DashStyles.Dot;
+                case DashStylesEnum.DashDot:
+                    return DashStyles.DashDot;
+                case DashStylesEnum.DashDotDot:
+                    return DashStyles.DashDotDot;
+                case DashStylesEnum.Solid:
+                    return DashStyles.Solid;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
---
+++ Mapping Tools/Classes/SnappingTools/Models/SnappingToolsPreferences.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.Windows.Media;
+using Mapping_Tools.Classes.MathUtil;
+using Mapping_Tools.Classes.SystemTools;
+
+namespace Mapping_Tools.Classes.SnappingTools {
+    public class SnappingToolsPreferences : BindableBase, ICloneable{
+        #region private storage
+        private List<RelevantObjectPreferences> releventObjectPreferences = new List<RelevantObjectPreferences> {
+            new RelevantObjectPreferences {
+                Name = "Virtual point preferences",
+                Color = Colors.Cyan,
+                Dashstyle = DashStylesEnum.Solid,
+                Opacity = 0.8,
+                Size = 5,
+                Thickness = 3,
+                HasSizeOption = true,
+            },
+            new RelevantObjectPreferences {
+                Name = "Virtual line preferences",
+                Color = Colors.LawnGreen,
+                Dashstyle = DashStylesEnum.Dash,
+                Opacity = 0.8,
+                Thickness = 3,
+                HasSizeOption = false,
+            },
+            new RelevantObjectPreferences {
+                Name = "Virtual circle preferences",
+                Color = Colors.Red,
+                Dashstyle = DashStylesEnum.Dash,
+                Opacity = 0.8,
+                Thickness = 3,
+                HasSizeOption = false,
+            }
+        };
+
+        private Hotkey snapHotkey = new Hotkey(Key.M, ModifierKeys.None);
+        private double offsetLeft = 2;
+        private double offsetTop = 1;
+        private double offsetRight = 2;
+        private double offsetBottom = 1;
+        private bool debugEnabled = false;
+        #endregion
+
+        public List<RelevantObjectPreferences> RelevantObjectPreferences {
+            get => releventObjectPreferences;
+            set => Set(ref releventObjectPreferences, value);
+        }
+
+        #region global settings
+        public Hotkey SnapHotkey {
+            get => snapHotkey;
+            set => Set(ref snapHotkey, value);
+        }
+
+        public double OffsetLeft {
+            get => offsetLeft;
+            set => Set(ref offsetLeft, value);
+        }
+
+        public double OffsetTop {
+            get => offsetTop;
+            set => Set(ref offsetTop, value);
+        }
+
+        public double OffsetRight {
+            get => offsetRight;
+            set => Set(ref offsetRight, value);
+        }
+
+        public double OffsetBottom {
+            get => offsetBottom;
+            set => Set(ref offsetBottom, value);
+        }
+
+        public Box2 OverlayOffset => new Box2(OffsetLeft, OffsetTop, OffsetRight, OffsetBottom);
+
+        public bool DebugEnabled {
+            get => debugEnabled;
+            set => Set(ref debugEnabled, value);
+        }
+
+        #endregion
+
+        #region helper methods
+        /// <summary>
+        /// Finds and returns an existing instance of <see cref="RelevantObjectPreferences"/> based on <see cref="RelevantObjectPreferences.Name"/> property.
+        /// </summary>
+        /// <param name="input"><see cref="RelevantObjectPreferences.Name"/> property of the desired instance of <see cref="RelevantObjectPreferences.Name"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public RelevantObjectPreferences GetReleventObjectPreferences(string input) {
+            List<RelevantObjectPreferences> output = releventObjectPreferences.Where(o => o.Name == input).ToList();
+            return output[0] ?? throw new ArgumentOutOfRangeException();
+        }
+        #endregion
+
+        #region IClonable members
+        public object Clone() {
+            return MemberwiseClone();
+        }
+        #endregion
+
+        public void CopyTo(SnappingToolsPreferences other) {
+            foreach (var prop in typeof(SnappingToolsPreferences).GetProperties()) {
+                if (!prop.CanWrite || !prop.CanRead) continue;
+                try { prop.SetValue(other, prop.GetValue(this)); } catch { }
+            }
+        }
+    }
+}
