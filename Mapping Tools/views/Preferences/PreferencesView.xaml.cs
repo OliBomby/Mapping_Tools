72c201f74f50eaa138f11797fa7f1efdaf10af72
Olivier Schipper <olivierschipper@gmail.com>
2019-09-29 23:19:44 +0200

    "Enabled" light mode

--- Mapping Tools/views/Preferences/PreferencesView.xaml.cs
+++ Mapping Tools/views/Preferences/PreferencesView.xaml.cs
@@ -17,13 +17,80 @@ namespace Mapping_Tools.Views
             DataContext = SettingsManager.Settings;
         }
 
+        [Obsolete]
         private void MakeDark(object sender, RoutedEventArgs e) {
-            //new PaletteHelper().SetLightDark(true);
+            new PaletteHelper().SetLightDark(true);
         }
 
+        [Obsolete]
         private void MakeLight(object sender, RoutedEventArgs e) {
-            MessageBox.Show("No", "No");
-            //new PaletteHelper().SetLightDark(false);
+            if (MessageBox.Show("No", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("No.", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("No..", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("You really don't want this.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Just look at this message box.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("It's terrible.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Your eyes must be hurting already.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Just.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Why.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("You WILL regret this.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("This is a bad idea.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("You will literally punch your own eyes.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("You're probably thinking I have no idea what I'm talking about.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("I've been through this before.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("You hated it so much that you uninstalled me.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Please.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("I have a family.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Don't make me do this.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("OH god.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Don't blame me after this.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show(".", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("This is your last warning.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("...", "No", MessageBoxButton.YesNo) != MessageBoxResult.Yes) return;
+            if (MessageBox.Show("Goodbye cruel world.", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+            if (MessageBox.Show("I thought we could be friends...", "No", MessageBoxButton.YesNo) !=
+                MessageBoxResult.Yes) return;
+
+            new PaletteHelper().SetLightDark(false);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
