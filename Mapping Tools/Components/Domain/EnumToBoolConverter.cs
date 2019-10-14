1ea8f6c3e04d880f692428bfdd3ad4bfc0dfa66f
OliBomby <olivierschipper@gmail.com>
2019-10-14 12:25:58 +0200

    some pepega forgot to include EnumToBoolConverter

--- Mapping Tools/Components/Domain/EnumToBoolConverter.cs
+++ Mapping Tools/Components/Domain/EnumToBoolConverter.cs
@@ -2,14 +2,14 @@
 using System.Globalization;
 using System.Windows.Data;
 
-namespace Mapping_Tools.Components.Domain{
+namespace Mapping_Tools.Components.Domain {
     public class EnumToBoolConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             return value.Equals(parameter);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            return ((bool)value) ? parameter : Binding.DoNothing;
+            return ((bool) value) ? parameter : Binding.DoNothing;
         }
     }
 }
