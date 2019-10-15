6f2fd90f7dee0909b8b5e07e227edea25e9cfc8b
OliBomby <olivierschipper@gmail.com>
2019-10-15 15:55:05 +0200

    more generic generator collection 2

--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorCollection/RelevantObjectGeneratorCollection.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorCollection/RelevantObjectGeneratorCollection.cs
@@ -6,40 +6,5 @@ using Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators
 
 namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorCollection {
     public class RelevantObjectGeneratorCollection : GeneratorCollection {
-        /// <summary>
-        /// Generates new objects for the next layer based on the new object in the previous layer.
-        /// </summary>
-        /// <param name="thisLayer"></param>
-        /// <param name="nextLayer">The layer to generate new objects for</param>
-        /// <param name="nextContext">Context of the next layer</param>
-        /// <param name="newHitObject">The new object of the previous layer</param>
-        public override void GenerateNewObjects(ObjectLayer thisLayer, ObjectLayer nextLayer, IRelevantObject newHitObject) {
-            // Only generate objects using the new object and the rest and redo all concurrent generators
-
-            // Get the dependencies of all the active generators
-            // Check the preferences for generating preferences
-            // Get the necessary layers
-            // Take into account the situation. The changes of the layer
-            // Generate pairs with a set of layers and a dependency
-            // Return the new pairs
-            
-            var activeGenerators = GetActiveGenerators();
-            foreach (var generator in activeGenerators) {
-                var method = generator.GetGeneratorMethod();
-                var dependencies = generator.GetDependencies();
-                //var concurrent = generator.IsConcurrent;
-                //var needsHitObjects = generator.NeedsHitObjects();
-
-                var parametersList = RelevantObjectPairGenerator.GetParametersList(dependencies, thisLayer.Objects, newHitObject);
-                
-                foreach (var parameters in parametersList) {
-                    nextLayer.Add(method.Invoke(generator, parameters));
-                }
-            }
-        }
-
-        public override void GenerateNewObjects(ObjectLayer thisLayer, ObjectLayer nextLayer, IRelevantObject[] newRelevantObjects) {
-            throw new System.NotImplementedException();
-        }
     }
 }
