a4ad844aa03b29af88b953443616eb8090e8e9fd
OliBomby <olivierschipper@gmail.com>
2019-10-19 17:30:00 +0200

    start fixing generators to new structure

--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorTypes/IGeneratePointsfromRelevantObjects.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObject;
-
-namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorTypes {
-    public interface IGeneratePointsFromRelevantObjects {
-        List<RelevantPoint> GetRelevantObjects(List<RelevantPoint> points, List<RelevantLine> lines, List<RelevantCircle> circles);
-    }
-}
--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/Generators/AnchorPointGenerator.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/Generators/AnchorPointGenerator.cs
@@ -2,17 +2,20 @@
 using System.Linq;
 using Mapping_Tools.Classes.BeatmapHelper;
 using Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObject;
+using Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.Allocation;
 using Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorTypes;
 
 namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.Generators {
-    class AnchorPointGenerator : RelevantObjectsGenerator, IGeneratePointsFromHitObjects {
+    class AnchorPointGenerator : RelevantObjectsGenerator {
         public override string Name => "Virtual Points on Slider Anchors";
         public override string Tooltip => "Generates virtual points on the anchor points of sliders.";
         public override GeneratorType GeneratorType => GeneratorType.Generators;
 
-        public List<RelevantPoint> GetRelevantObjects(List<HitObject> objects) {
-            return objects.Where(ho => ho.IsSlider)
-                .SelectMany(ho => ho.GetAllCurvePoints().Select(poi => new RelevantPoint(poi))).ToList();
+        [RelevantObjectGenerator]
+        public IEnumerable<RelevantPoint> GetRelevantObjects(RelevantHitObject relevantHitObject) {
+            return relevantHitObject.HitObject.IsSlider
+                ? relevantHitObject.HitObject.GetAllCurvePoints().Select(o => new RelevantPoint(o))
+                : new RelevantPoint[0];
         }
     }
 }
