c381d5bb98c23e850059568eddaa19ce6a71f468
OliBomby <olivierschipper@gmail.com>
2019-11-13 10:18:01 +0100

    fixed double & int converters & added custom converters to geometry dashboard

--- Mapping Tools/Components/Domain/IntToStringConverter.cs
+++ Mapping Tools/Components/Domain/IntToStringConverter.cs
@@ -5,28 +5,28 @@ using System.Windows.Data;
 using Mapping_Tools.Classes.SystemTools;
 
 namespace Mapping_Tools.Components.Domain {
-    internal class DoubleToStringConverter : IValueConverter {
+    internal class IntToStringConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (value != null) return ((double) value).ToString(CultureInfo.InvariantCulture);
+            if (value != null) return ((int) value).ToString(CultureInfo.InvariantCulture);
             return parameter != null ? parameter.ToString() : "";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
             if (value == null) {
                 if (parameter != null) {
-                    return (double) parameter;
+                    return (int) parameter;
                 }
                 return new ValidationResult(false, "Cannot convert back null.");
             }
 
             if (parameter == null) {
-                if (TypeConverters.TryParseDouble(value.ToString(), out double result1)) {
+                if (TypeConverters.TryParseInt(value.ToString(), out int result1)) {
                     return result1;
                 }
 
-                return new ValidationResult(false, "Double format error.");
+                return new ValidationResult(false, "Int format error.");
             }
-            TypeConverters.TryParseDouble(value.ToString(), out double result2, (double)parameter);
+            TypeConverters.TryParseInt(value.ToString(), out int result2, (int)parameter);
             return result2;
         }
     }
