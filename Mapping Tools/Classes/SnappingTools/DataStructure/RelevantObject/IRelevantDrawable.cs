f09c0458b00b69423c7bd37535e17052c788b444
OliBomby <olivierschipper@gmail.com>
2019-11-13 18:28:12 +0100

    changed render of selected objects & improved preferences fetching

--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObject/IRelevantDrawable.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObject/IRelevantDrawable.cs
@@ -3,9 +3,11 @@ using Mapping_Tools.Classes.MathUtil;
 
 namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObject {
     public interface IRelevantDrawable : IRelevantObject {
+        string PreferencesName { get; }
         double DistanceTo(Vector2 point);
         Vector2 NearestPoint(Vector2 point);
         bool Intersection(IRelevantObject other, out Vector2[] intersections);
         void DrawYourself(DrawingContext context, CoordinateConverter converter, SnappingToolsPreferences preferences);
+        void DrawYourself(DrawingContext context, CoordinateConverter converter, RelevantObjectPreferences preferences, Pen pen);
     }
 }
--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObject/RelevantDrawable.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObject/RelevantDrawable.cs
@@ -4,34 +4,58 @@ using MaterialDesignColors.ColorManipulation;
 
 namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObject {
     public abstract class RelevantDrawable : RelevantObject, IRelevantDrawable {
+        public abstract string PreferencesName { get; }
+
         public abstract double DistanceTo(Vector2 point);
 
         public abstract Vector2 NearestPoint(Vector2 point);
 
         public abstract bool Intersection(IRelevantObject other, out Vector2[] intersections);
 
-        public abstract void DrawYourself(DrawingContext context, CoordinateConverter converter,
-            SnappingToolsPreferences preferences);
+        public virtual void DrawYourself(DrawingContext context, CoordinateConverter converter, SnappingToolsPreferences preferences) {
+            var relevantObjectPreferences = GetRelevantObjectPreferences(preferences);
+            if (IsSelected) {
+                DrawYourself(context, converter, relevantObjectPreferences, GetPenSelected(relevantObjectPreferences));
+            }
+            DrawYourself(context, converter, relevantObjectPreferences, GetPen(relevantObjectPreferences));
+        }
+
+        public abstract void DrawYourself(DrawingContext context, CoordinateConverter converter, RelevantObjectPreferences preferences, Pen pen);
+
+        protected RelevantObjectPreferences GetRelevantObjectPreferences(SnappingToolsPreferences preferences) {
+            return preferences.GetReleventObjectPreferences(PreferencesName);
+        }
 
         protected Pen GetPen(RelevantObjectPreferences preferences) {
             return new Pen {
                 Brush = new SolidColorBrush {
                     Color = AdjustColor(preferences.Color),
-                    Opacity = AdjustOpacity(preferences.Opacity),
+                    Opacity = AdjustOpacity(preferences.Opacity)
                 },
                 DashStyle = preferences.GetDashStyle(),
-                Thickness = preferences.Thickness,
+                Thickness = preferences.Thickness
             };
         }
 
-        protected Color AdjustColor(Color color) {
+        protected Pen GetPenSelected(RelevantObjectPreferences preferences) {
+            return new Pen {
+                Brush = new SolidColorBrush {
+                    Color = Color.FromArgb(255, 255, 200, 0),
+                    Opacity = 1
+                },
+                DashStyle = DashStyles.Solid,
+                Thickness = preferences.Thickness + 2
+            };
+        }
+
+        private Color AdjustColor(Color color) {
             var hsb = color.ToHsb();
-            return new Hsb( IsSelected ? hsb.Hue - 50 : hsb.Hue,
+            return new Hsb( hsb.Hue,
                 IsLocked ? IsSelected ? hsb.Saturation * 0.6 : hsb.Saturation * 0.3 : hsb.Saturation,
                 IsInheritable ? hsb.Brightness : hsb.Brightness * 0.5).ToColor();
         }
 
-        protected double AdjustOpacity(double opacity) {
+        private double AdjustOpacity(double opacity) {
             return Relevancy * opacity;
         }
     }
