5a4ba44bfe8e9a3397c5610953881dfdd228ed0c
OliBomby <olivierschipper@gmail.com>
2019-11-15 13:55:40 +0100

    Named and sorted generator settings

--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorSettingses/SliderPathGeneratorSettings.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorSettingses/SliderPathGeneratorSettings.cs
@@ -3,6 +3,7 @@
 namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorSettingses {
     public class SliderPathGeneratorSettings : GeneratorSettings {
         private double _pointDensity;
+        [DisplayName("Point Density")]
         [Description("Specifies how many points will be generated per osu! pixel of sliderpath.")]
         public double PointDensity {
             get => _pointDensity;
--- Mapping Tools/views/SnappingTools/GeneratorSettingsWindow.xaml.cs
+++ Mapping Tools/views/SnappingTools/GeneratorSettingsWindow.xaml.cs
@@ -32,50 +32,84 @@ namespace Mapping_Tools.Views.SnappingTools {
         }
 
         private void PopulateSettings(object settings) {
-            foreach (var prop in settings.GetType().GetProperties()) {
-                if (!prop.CanWrite || !prop.CanRead) continue;
-                if (!SettingsTypes.Contains(prop.PropertyType)) continue;
+            var type = settings.GetType();
+            if (type.BaseType == typeof(GeneratorSettings)) {
+                var sharedProperties = type.BaseType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                var uniqueProperties = type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public);
 
-                var value = prop.GetValue(settings);
-                if (value == null) continue;
+                CreateCard(sharedProperties, settings);
+                CreateCard(uniqueProperties, settings);
+            } else {
+                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-                var horizontalPanel = new StackPanel {Orientation = Orientation.Horizontal, Margin = new Thickness(10)};
-
-                var name = new TextBlock {Text = prop.Name, Width = 150};
-                if (prop.GetCustomAttribute(typeof(DescriptionAttribute)) is DescriptionAttribute d) {
-                    name.ToolTip = d.Description;
-                }
-                horizontalPanel.Children.Add(name);
-
-                switch (value) {
-                    case bool boolValue:
-                        var toggleButton = new ToggleButton {IsChecked = boolValue, Cursor = Cursors.Hand};
-                        Binding toggleBinding = new Binding(prop.Name) {
-                            Source = DataContext
-                        };
-                        toggleButton.SetBinding(ToggleButton.IsCheckedProperty, toggleBinding);
-                        horizontalPanel.Children.Add(toggleButton);
-                        break;
-                    case double doubleValue:
-                        var doubleTextBox = new TextBox {Width = 100};
-                        Binding doubleBinding = new Binding(prop.Name) {
-                            Source = DataContext,
-                            Converter = new DoubleToStringConverter()
-                        };
-                        doubleTextBox.SetBinding(TextBox.TextProperty, doubleBinding);
-                        horizontalPanel.Children.Add(doubleTextBox);
-                        break;
-                    case string stringValue:
-                        var stringTextBox = new TextBox {Width = 100};
-                        Binding stringBinding = new Binding(prop.Name) {
-                            Source = DataContext
-                        };
-                        stringTextBox.SetBinding(TextBox.TextProperty, stringBinding);
-                        horizontalPanel.Children.Add(stringTextBox);
-                        break;
-                }
-                Panel.Children.Add(horizontalPanel);
+                CreateCard(properties, settings);
             }
+        }
+
+        private void CreateCard(IReadOnlyCollection<PropertyInfo> props, object settings) {
+            if (props.Count == 0) return;
+            var card = new MaterialDesignThemes.Wpf.Card {Margin = new Thickness(10)};
+            var panel = new StackPanel();
+            card.Content = panel;
+
+            foreach (var prop in props) {
+                var e = GetSettingControl(prop, settings);
+                if (e != null)
+                    panel.Children.Add(e);
+            }
+
+            Panel.Children.Add(card);
+        }
+
+        private StackPanel GetSettingControl(PropertyInfo prop, object settings) {
+            if (!prop.CanWrite || !prop.CanRead) return null;
+            if (!SettingsTypes.Contains(prop.PropertyType)) return null;
+
+            var value = prop.GetValue(settings);
+            if (value == null) return null;
+
+            var horizontalPanel = new StackPanel {Orientation = Orientation.Horizontal, Margin = new Thickness(10)};
+
+            var name = new TextBlock {Width = 150};
+            if (prop.GetCustomAttribute(typeof(DisplayNameAttribute)) is DisplayNameAttribute n) {
+                name.Text = n.DisplayName;
+            } else {
+                name.Text = prop.Name;
+            }
+            if (prop.GetCustomAttribute(typeof(DescriptionAttribute)) is DescriptionAttribute d) {
+                name.ToolTip = d.Description;
+            }
+            horizontalPanel.Children.Add(name);
+
+            switch (value) {
+                case bool _:
+                    var toggleButton = new ToggleButton {Cursor = Cursors.Hand};
+                    Binding toggleBinding = new Binding(prop.Name) {
+                        Source = DataContext
+                    };
+                    toggleButton.SetBinding(ToggleButton.IsCheckedProperty, toggleBinding);
+                    horizontalPanel.Children.Add(toggleButton);
+                    break;
+                case double _:
+                    var doubleTextBox = new TextBox {Width = 100};
+                    Binding doubleBinding = new Binding(prop.Name) {
+                        Source = DataContext,
+                        Converter = new DoubleToStringConverter()
+                    };
+                    doubleTextBox.SetBinding(TextBox.TextProperty, doubleBinding);
+                    horizontalPanel.Children.Add(doubleTextBox);
+                    break;
+                case string _:
+                    var stringTextBox = new TextBox {Width = 100};
+                    Binding stringBinding = new Binding(prop.Name) {
+                        Source = DataContext
+                    };
+                    stringTextBox.SetBinding(TextBox.TextProperty, stringBinding);
+                    horizontalPanel.Children.Add(stringTextBox);
+                    break;
+            }
+
+            return horizontalPanel;
         }
 
         private void ApplyButton_Click(object sender, RoutedEventArgs e)
