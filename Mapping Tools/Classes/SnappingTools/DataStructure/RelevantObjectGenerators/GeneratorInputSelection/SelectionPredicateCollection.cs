80f6483106cbea4fc36737c4401c8f731ef4995f
Olivier Schipper <olivierschipper@gmail.com>
2019-11-17 17:37:12 +0100

    made selection predicate

---
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorInputSelection/SelectionPredicateCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorInputSelection {
+    public class SelectionPredicateCollection {
+        public ObservableCollection<SelectionPredicate> Predicates { get; set; }
+
+    }
+}
