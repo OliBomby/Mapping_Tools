61885e13f33229e2db54dd4d83333427dd9b6eef
OliBomby <olivierschipper@gmail.com>
2019-11-18 12:32:54 +0100

    added symmetry generator

---
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorSettingses/SymmetryGeneratorSettings.cs
@@ -0,0 +1,6 @@
+﻿using System.ComponentModel;
+
+namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenerators.GeneratorSettingses {
+    public class SymmetryGeneratorSettings : GeneratorSettings {
+    }
+}
