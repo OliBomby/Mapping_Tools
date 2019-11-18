65f9820d4df679fb246755e53f6fe0a2e6145df9
OliBomby <olivierschipper@gmail.com>
2019-11-18 23:20:34 +0100

    added GeneratedNotByThis predicate

--- Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorInputSelection/SelectionPredicate.cs
+++ Mapping Tools/Classes/SnappingTools/DataStructure/RelevantObjectGenerators/GeneratorInputSelection/SelectionPredicate.cs
@@ -6,10 +6,12 @@ namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenera
         private bool _needSelected;
         private bool _needLocked;
         private bool _needGeneratedByThis;
+        private bool _needGeneratedNotByThis;
         private double _minRelevancy;
         public bool NeedSelected { get => _needSelected; set => Set(ref _needSelected, value); }
         public bool NeedLocked { get => _needLocked; set => Set(ref _needLocked, value); }
         public bool NeedGeneratedByThis { get => _needGeneratedByThis; set => Set(ref _needGeneratedByThis, value); }
+        public bool NeedGeneratedNotByThis { get => _needGeneratedNotByThis; set => Set(ref _needGeneratedNotByThis, value); }
         public double MinRelevancy { get => _minRelevancy; set => Set(ref _minRelevancy, value); }
 
         public SelectionPredicate() {
@@ -19,11 +21,12 @@ namespace Mapping_Tools.Classes.SnappingTools.DataStructure.RelevantObjectGenera
             if (NeedSelected && !relevantObject.IsSelected) return false;
             if (NeedLocked && !relevantObject.IsLocked) return false;
             if (NeedGeneratedByThis && relevantObject.Generator != null && relevantObject.Generator != generator) return false;
+            if (NeedGeneratedNotByThis && relevantObject.Generator != null && relevantObject.Generator == generator) return false;
             return !(relevantObject.Relevancy < MinRelevancy);
         }
 
         public override string ToString() {
-            return $@"NeedSelected: {NeedSelected}, NeedLocked: {NeedLocked}, NeedGeneratedByThis: {NeedGeneratedByThis}, MinRelevancy: {MinRelevancy}";
+            return $@"NeedSelected: {NeedSelected}, NeedLocked: {NeedLocked}, NeedGeneratedByThis: {NeedGeneratedByThis}, NeedGeneratedNotByThis: {NeedGeneratedNotByThis}, MinRelevancy: {MinRelevancy}";
         }
     }
 }
