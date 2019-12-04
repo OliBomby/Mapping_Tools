85887abf19d140f945cfdda7facd3933f11a404e
OliBomby <olivierschipper@gmail.com>
2019-12-04 09:30:28 +0100

    added combo colour studio files

---
+++ Mapping Tools/viewmodels/ComboColourStudioVm.cs
@@ -0,0 +1,83 @@
+﻿using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Components.Domain;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Mapping_Tools.Viewmodels {
+
+    public class ComboColourStudioVm : BindableBase {
+        private ObservableCollection<SpecialColour> _comboColours;
+        private ObservableCollection<SpecialColour> _specialColours;
+
+        public ComboColourStudioVm() {
+            ComboColours = new ObservableCollection<SpecialColour>();
+            SpecialColours = new ObservableCollection<SpecialColour>();
+
+            AddCommand = new CommandImplementation(_ => {
+                if (ComboColours.Count >= 8) return;
+                ComboColours.Add(ComboColours.Count > 0
+                    ? new SpecialColour(ComboColours[ComboColours.Count - 1].Color)
+                    : new SpecialColour(Colors.White));
+            });
+
+            RemoveCommand = new CommandImplementation(_ => {
+                if (ComboColours.Count > 0) {
+                    ComboColours.RemoveAt(ComboColours.Count - 1);
+                }
+            });
+
+            AddSpecialCommand = new CommandImplementation(_ => {
+                SpecialColours.Add(SpecialColours.Count > 0
+                    ? new SpecialColour(SpecialColours[SpecialColours.Count - 1].Color)
+                    : new SpecialColour(Colors.White));
+            });
+
+            RemoveSpecialCommand = new CommandImplementation(_ => {
+                if (SpecialColours.Count > 0) {
+                    SpecialColours.RemoveAt(SpecialColours.Count - 1);
+                }
+            });
+        }
+
+        private void ImportFromBeatmap(string importPath) {
+            try {
+                var editor = new BeatmapEditor(importPath);
+                var beatmap = editor.Beatmap;
+
+                ComboColours.Clear();
+                for (int i = 0; i < beatmap.ComboColours.Count; i++) {
+                    ComboColours.Add(new SpecialColour(beatmap.ComboColours[i].Color, $"Combo{i}"));
+                }
+                SpecialColours.Clear();
+                foreach (var specialColour in beatmap.SpecialColours) {
+                    SpecialColours.Add(new SpecialColour(specialColour.Value.Color, specialColour.Key));
+                }
+            }
+            catch( Exception ex ) {
+                MessageBox.Show($"{ex.Message}{Environment.NewLine}{ex.StackTrace}", "Error");
+            }
+        }
+
+
+        public ObservableCollection<SpecialColour> ComboColours {
+            get => _comboColours;
+            set => Set(ref _comboColours, value);
+        }
+
+        public ObservableCollection<SpecialColour> SpecialColours {
+            get => _specialColours;
+            set => Set(ref _specialColours, value);
+        }
+
+        public string ExportPath { get; set; }
+
+        public CommandImplementation AddCommand { get; }
+        public CommandImplementation RemoveCommand { get; }
+        public CommandImplementation AddSpecialCommand { get; }
+        public CommandImplementation RemoveSpecialCommand { get; }
+    }
+}
---
+++ Mapping Tools/views/ComboColourStudio/ComboColourStudioView.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.Tools;
+using Mapping_Tools.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace Mapping_Tools.Views {
+    /// <summary>
+    /// Interactielogica voor ComboColourStudioView.xaml
+    /// </summary>
+    public partial class ComboColourStudioView : ISavable<ComboColourStudioVm> {
+        public string AutoSavePath => Path.Combine(MainWindow.AppDataPath, "combocolourproject.json");
+
+        public string DefaultSaveFolder => Path.Combine(MainWindow.AppDataPath, "Combo Colour Studio Projects");
+
+        public static readonly string ToolName = "Combo Colour Studio";
+
+        public static readonly string ToolDescription = $@"With Combo Colour Studio you can easily customize the combo colours of your beatmap.{Environment.NewLine}You define colored sections much like how you use timing points in the osu! editor. Just add a new colour point and define the sequence of combo colours.{Environment.NewLine}You can also define colour points which only work for one combo, so you can emphasize specific patterns using colour.";
+
+        private ComboColourStudioVm Settings => (ComboColourStudioVm) DataContext;
+
+        public ComboColourStudioView() {
+            InitializeComponent();
+            DataContext = new ComboColourStudioVm();
+            Width = MainWindow.AppWindow.content_views.Width;
+            Height = MainWindow.AppWindow.content_views.Height;
+            ProjectManager.LoadProject(this, message: false);
+        }
+
+        protected override void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e) {
+            var bgw = sender as BackgroundWorker;
+            e.Result = Copy_Metadata((ComboColourStudioVm) e.Argument, bgw, e);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e) {
+            var filesToCopy = ((MetadataManagerVm)DataContext).ExportPath.Split('|');
+            foreach (var fileToCopy in filesToCopy) {
+                IOHelper.SaveMapBackup(fileToCopy);
+            }
+
+            BackgroundWorker.RunWorkerAsync((MetadataManagerVm)DataContext);
+            CanRun = false;
+        }
+
+
+        private static string Copy_Metadata(ComboColourStudioVm arg, BackgroundWorker worker, DoWorkEventArgs _) {
+            var paths = arg.ExportPath.Split('|');
+            var mapsDone = 0;
+
+            var editorRead = EditorReaderStuff.TryGetFullEditorReader(out var reader);
+
+            foreach (var path in paths) {
+                var editor = editorRead ? EditorReaderStuff.GetNewestVersion(path, reader) : new BeatmapEditor(path);
+                var beatmap = editor.Beatmap;
+
+                beatmap.ComboColours = new List<ComboColour>(arg.ComboColours);
+                beatmap.SpecialColours.Clear();
+                foreach (var specialColour in arg.SpecialColours) {
+                    beatmap.SpecialColours.Add(specialColour.Name, specialColour);
+                }
+
+                // Save the file
+                editor.SaveFile();
+
+                // Update progressbar
+                if (worker != null && worker.WorkerReportsProgress) {
+                    worker.ReportProgress(++mapsDone * 100 / paths.Length);
+                }
+            }
+
+            // Make an accurate message
+            var message = $"Successfully exported metadata to {mapsDone} {(mapsDone == 1 ? "beatmap" : "beatmaps")}!";
+            return message;
+        }
+
+        public ComboColourStudioVm GetSaveData() {
+            return Settings;
+        }
+
+        public void SetSaveData(ComboColourStudioVm saveData) {
+            DataContext = saveData;
+        }
+    }
+}
