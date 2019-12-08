1fb5733038e0d974a05152d3677b9f322ef157b8
Olivier Schipper <olivierschipper@gmail.com>
2019-12-08 18:42:43 +0100

    improved TimeToStringConverter & implemented it in various text boxes

--- Mapping Tools/Components/Domain/TimeToStringConverter.cs
+++ Mapping Tools/Components/Domain/TimeToStringConverter.cs
@@ -1,37 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Data;
+using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.SystemTools;
 
-namespace Mapping_Tools.Components.Domain
-{
-    public class TimeToStringConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value != null) return string.Format("{0:mm\\:ss\\:fff}", TimeSpan.FromMilliseconds((double)value));
-            return parameter != null ? parameter.ToString() : "";
+namespace Mapping_Tools.Components.Domain {
+    public class TimeToStringConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (!(value is double timeValue)) {
+                return "";
+            }
+
+            try {
+                return parameter != null
+                    ? value.ToInvariant()
+                    : $"{TimeSpan.FromMilliseconds(timeValue):mm\\:ss\\:fff}";
+            } catch (OverflowException) {
+                return value.ToInvariant();
+            }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            try
-            {
-                return TimeSpan.ParseExact(((string)value).Substring(0, ((string)value).IndexOf("(") - 1),
-                                                @"mm\:ss\:fff", culture, TimeSpanStyles.AssumeNegative);
-            }
-            catch (FormatException)
-            {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            if (!(value is string str)) {
                 return null;
             }
-            catch (OverflowException) { return null; }
-            catch ( ArgumentNullException)
-            { return null; }
 
+            var bracketIndex = str.IndexOf("(", StringComparison.Ordinal);
+            if (TimeSpan.TryParseExact(str.Substring(0, bracketIndex == -1 ? str.Length : bracketIndex - 1).Trim(),
+                @"mm\:ss\:fff", culture, TimeSpanStyles.None, out var result)) {
+                return result.TotalMilliseconds;
+            }
 
+            if (parameter is string s) {
+                return TypeConverters.TryParseDouble(str, out double result2) ? result2 : double.Parse(s, CultureInfo.InvariantCulture);
+            }
+            if (TypeConverters.TryParseDouble(str, out double result3)) {
+                return result3;
+            }
+
+            return new ValidationResult(false, "Time format error.");
         }
     }
 }
