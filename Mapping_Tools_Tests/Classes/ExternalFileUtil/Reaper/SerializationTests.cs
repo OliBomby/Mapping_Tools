5f3a5ad4f78d771b28169d4f6ec9308949f874e3
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-12-08 20:07:45 +1000

    Tried to do tests

---
+++ Mapping_Tools_Tests/Classes/ExternalFileUtil/Reaper/SerializationTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mapping_Tools.Classes.ExternalFileUtil.Reaper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mapping_Tools.Classes.ExternalFileUtil.Reaper.Tests
+{
+    [TestClass()]
+    public class SerializationTests
+    {
+        [TestMethod()]
+        public void PopulateDictionaryTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
