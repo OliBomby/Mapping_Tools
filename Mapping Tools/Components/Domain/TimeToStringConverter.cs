239283f45049dda2ae89d70be7bd5827cb213650
Coppertine <37494321+Coppertine@users.noreply.github.com>
2019-12-09 02:01:17 +1000

    TimeToStringConverter. Oli, you do the rest

---
+++ Mapping Tools/Components/Domain/TimeToStringConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Mapping_Tools.Components.Domain
+{
+    public class TimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null) return string.Format("{0:mm\\:ss\\:fff}", TimeSpan.FromMilliseconds((double)value));
+            return parameter != null ? parameter.ToString() : "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string format = @"mm\:ss\:fff";
+            try
+            {
+                return TimeSpan.ParseExact(((string)value).Substring(0, ((string)value).IndexOf("(") - 1), format, culture, TimeSpanStyles.AssumeNegative);
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            catch (OverflowException) { return null; }
+            catch { ArgumentNullException}
+            { return null; }
+
+
+        }
+    }
+}
