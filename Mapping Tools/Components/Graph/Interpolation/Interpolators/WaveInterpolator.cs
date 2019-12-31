0054adce1a61eff75e5ceca3c5e8fc0a9c3e72cc
Olivier Schipper <olivierschipper@gmail.com>
2019-12-31 22:56:32 +0100

    implemented integration for wave interpolator

--- Mapping Tools/Components/Graph/Interpolation/Interpolators/WaveInterpolator.cs
+++ Mapping Tools/Components/Graph/Interpolation/Interpolators/WaveInterpolator.cs
@@ -4,7 +4,7 @@ using Mapping_Tools.Classes.MathUtil;
 
 namespace Mapping_Tools.Components.Graph.Interpolation.Interpolators {
     [DisplayName("Wave")]
-    public class WaveInterpolator : CustomInterpolator {
+    public class WaveInterpolator : CustomInterpolator, IIntegrableInterpolator {
         public string Name => "Wave";
 
         public WaveInterpolator() {
@@ -20,12 +20,37 @@ namespace Mapping_Tools.Components.Graph.Interpolation.Interpolators {
         }
 
         private static double SineWave(double t) {
-            return (Math.Sin(t - Math.PI / 2) + 1) / 2;
+            return (-Math.Cos(t) + 1) / 2;
+        }
+
+        private static double SineWavePrimitive(double t, double c) {
+            return t / 2 - Math.Sin(2 * Math.PI * c * t) / (4 * Math.PI * c);
         }
 
         private static double TriangleWave(double t, double T) {
             var modT = t % T;
             return modT < T / 2 ? 2 * modT / T : 2 - 2 * modT / T;
+        }
+
+        /// <summary>
+        /// https://math.stackexchange.com/questions/178079/integration-of-sawtooth-square-and-triangle-wave-functions
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        private static double TriangleWaveIntegral(double t, double T) {
+            var modT = t % T;
+            var n = Math.Floor(t / T);
+            var integral = modT < T / 2 ? Math.Pow(modT, 2) / T : 2 * modT - Math.Pow(modT, 2) / T - T / 2;
+            return n * T * 0.5 + integral;
+        }
+
+        public double GetIntegral(double t1, double t2) {
+            var cycles = Math.Round((1 - Math.Abs(MathHelper.Clamp(P, -1, 1))) * 50) + 0.5;
+
+            return P < 0 ? 
+                TriangleWaveIntegral(t2, 1 / cycles) - TriangleWaveIntegral(t1, 1 / cycles) : 
+                SineWavePrimitive(t2, cycles) - SineWavePrimitive(t1, cycles);
         }
     }
 }
