75becad6534e7a02cf6823c71ba3519080ea7505
OliBomby <olivierschipper@gmail.com>
2020-02-04 12:02:05 +0100

    made some dialog files

---
+++ Mapping Tools/views/HitsoundStudio/HitsoundStudioExportDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows;
+
+namespace Mapping_Tools.Views {
+    /// <summary>
+    /// Interaction logic for HitsoundStudioExportDialog.xaml
+    /// </summary>
+    public partial class HitsoundStudioExportDialog {
+        public HitsoundStudioExportDialog(double initialValue = 0) {
+            InitializeComponent();
+            ValueBox.Text = initialValue.ToString(CultureInfo.InvariantCulture);
+        }
+
+        private void TypeValueDialog_OnLoaded(object sender, RoutedEventArgs e) {
+            ValueBox.Focus();
+            ValueBox.SelectAll();
+        }
+    }
+}
