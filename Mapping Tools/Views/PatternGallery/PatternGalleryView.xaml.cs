fb973454e1b50ca9aebe9d68bf4a278d6d52f94a
OliBomby <olivierschipper@gmail.com>
2020-08-06 22:09:02 +0200

    made files for Pattern Gallery

---
+++ Mapping Tools/Views/PatternGallery/PatternGalleryView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.HitsoundStuff;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.SystemTools.QuickRun;
+using Mapping_Tools.Classes.Tools;
+using Mapping_Tools.Viewmodels;
+
+namespace Mapping_Tools.Views.PatternGallery
+{
+    /// <summary>
+    /// Interactielogica voor PatternGalleryView.xaml
+    /// </summary>
+    [SmartQuickRunUsage(SmartQuickRunTargets.Always)]
+    public partial class PatternGalleryView : ISavable<PatternGalleryVm>, IQuickRun
+    {
+        public event EventHandler RunFinished;
+
+        public string AutoSavePath => Path.Combine(MainWindow.AppDataPath, "patterngalleryproject.json");
+
+        public string DefaultSaveFolder => Path.Combine(MainWindow.AppDataPath, "Pattern Gallery Projects");
+
+        public static readonly string ToolName = "Pattern Gallery";
+        public static readonly string ToolDescription =
+            $@"Save and load patterns from osu! beatmaps.";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public PatternGalleryView()
+        {
+            InitializeComponent();
+            DataContext = new PatternGalleryVm();
+            Width = MainWindow.AppWindow.content_views.Width;
+            Height = MainWindow.AppWindow.content_views.Height;
+            ProjectManager.LoadProject(this, message: false);
+        }
+
+        protected override void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var bgw = sender as BackgroundWorker;
+            e.Result = ExportPattern((PatternGalleryVm) e.Argument, bgw, e);
+        }
+
+        private string ExportPattern(PatternGalleryVm args, BackgroundWorker worker, DoWorkEventArgs _) {
+            return "yes";
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            RunTool(MainWindow.AppWindow.GetCurrentMaps(), quick: false);
+        }
+
+        public void QuickRun()
+        {
+            RunTool(new[] {IOHelper.GetCurrentBeatmapOrCurrentBeatmap()}, quick: true);
+        }
+
+
+        private void RunTool(string[] paths, bool quick = false)
+        {
+            if (!CanRun) return;
+
+            // Remove logical focus to trigger LostFocus on any fields that didn't yet update the ViewModel
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(this), null);
+
+            BackupManager.SaveMapBackup(paths);
+
+            BackgroundWorker.RunWorkerAsync(DataContext);
+
+            CanRun = false;
+        }
+
+        public PatternGalleryVm GetSaveData()
+        {
+            return (PatternGalleryVm) DataContext;
+        }
+
+        public void SetSaveData(PatternGalleryVm saveData)
+        {
+            DataContext = saveData;
+
+        }
+
+    }
+}
