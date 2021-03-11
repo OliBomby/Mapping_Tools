836f9156975cda2558f80950b945cf873eb2d009
OliBomby <olivierschipper@gmail.com>
2021-03-12 00:57:56 +0100

    added test project for core tests

---
+++ Mapping_Tools_Core_Tests/InputParsersTests.cs
@@ -0,0 +1,16 @@
+﻿using Mapping_Tools_Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Mapping_Tools_Core_Tests {
+    [TestClass]
+    public class SerializationTests {
+        [TestMethod]
+        public void ParseDoubleTest() {
+            double actual = InputParsers.ParseDouble("1 + 1");
+            const double expected = 2;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
