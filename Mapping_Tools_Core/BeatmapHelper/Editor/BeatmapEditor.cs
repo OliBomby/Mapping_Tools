2a9c5e199e671932f11aa3cde051e782cb3c4a70
OliBomby <olivierschipper@gmail.com>
2021-03-17 13:18:48 +0100

    done hitsound exporter class

--- Mapping_Tools_Core/BeatmapHelper/Editor/BeatmapEditor.cs
+++ Mapping_Tools_Core/BeatmapHelper/Editor/BeatmapEditor.cs
@@ -11,9 +11,10 @@ namespace Mapping_Tools_Core.BeatmapHelper.Editor {
         /// Saves the beatmap using <see cref=".SaveFile()"/> but also updates the filename according to the metadata of the <see cref="Beatmap"/>
         /// </summary>
         /// <remarks>This method also updates the Path property</remarks>
-        public void WriteFileWithNameUpdate(Beatmap beatmap) {
+        public void WriteFileWithNameUpdate(Beatmap beatmap, bool deleteOriginal = true) {
             // Remove the beatmap with the old filename
-            File.Delete(Path);
+            if (deleteOriginal)
+                File.Delete(Path);
 
             // Save beatmap with the new filename
             Path = System.IO.Path.Combine(GetParentFolder(), beatmap.GetFileName());
--- Mapping_Tools_Core/Tools/HitsoundStudio/Exporting/IHitsoundExporter.cs
+++ Mapping_Tools_Core/Tools/HitsoundStudio/Exporting/IHitsoundExporter.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using Mapping_Tools_Core.BeatmapHelper;
 using Mapping_Tools_Core.Tools.HitsoundStudio.Model;
 
 namespace Mapping_Tools_Core.Tools.HitsoundStudio.Exporting {
     public interface IHitsoundExporter {
-        void ExportHitsounds(ICollection<IHitsoundEvent> hitsounds);
+        void ExportHitsounds(ICollection<IHitsoundEvent> hitsounds, Beatmap beatmap);
     }
 }
