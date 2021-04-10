ae2887405d54843ef71968968620dad4c06336d4
OliBomby <olivierschipper@gmail.com>
2021-04-10 16:00:42 +0200

    Revert "Delete HoldNote.cs"
    This reverts commit 1e0c621825e49ae59df9e4043273fb8236ca09b6.

---
+++ Mapping_Tools_Core/BeatmapHelper/Objects/HoldNote.cs
@@ -0,0 +1,18 @@
+﻿using Mapping_Tools_Core.BeatmapHelper.Types;
+
+namespace Mapping_Tools_Core.BeatmapHelper.Objects {
+    public class HoldNote : HitObject, IHasDuration {
+        public double Duration {
+            get => EndTime - StartTime;
+            set => EndTime = StartTime + value;
+        }
+
+        public double EndTime { get; set; }
+
+        public HoldNote() {
+
+        }
+
+        protected override void DeepCloneAdd(HitObject clonedHitObject) { }
+    }
+}
