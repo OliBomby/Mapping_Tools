72f8d0553fb72ebaee338763f9a894198f71e026
OliBomby <olivierschipper@gmail.com>
2021-04-10 16:00:54 +0200

    Delete ManiaNote.cs

--- Mapping_Tools_Core/BeatmapHelper/Objects/ManiaNote.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Mapping_Tools_Core.BeatmapHelper.Objects {
-    public class ManiaNote : HitObject {
-        protected override void DeepCloneAdd(HitObject clonedHitObject) { }
-    }
-}
