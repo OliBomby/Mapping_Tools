0b9ca691a695fe079556454adfd191a78bdd7dfa
OliBomby <olivierschipper@gmail.com>
2021-04-10 14:00:08 +0200

    Create Spinner.cs

---
+++ Mapping_Tools_Core/BeatmapHelper/Objects/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using Mapping_Tools_Core.BeatmapHelper.Types;
+
+namespace Mapping_Tools_Core.BeatmapHelper.Objects {
+    public class Spinner : HitObject, IHasDuration {
+        public double Duration {
+            get => EndTime - StartTime;
+            set => EndTime = StartTime + value;
+        }
+
+        public double EndTime { get; set; }
+
+        public Spinner() {
+
+        }
+
+        protected override void DeepCloneAdd(HitObject clonedHitObject) { }
+    }
+}
