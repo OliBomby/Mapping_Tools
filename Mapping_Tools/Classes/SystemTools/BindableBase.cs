9ade2fe0f5288ef8f6af958baa5d1be6bfde5744
OliBomby <olivierschipper@gmail.com>
2021-05-24 13:02:59 +0200

    Update BindableBase.cs

--- Mapping_Tools/Classes/SystemTools/BindableBase.cs
+++ Mapping_Tools/Classes/SystemTools/BindableBase.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Mapping_Tools.Classes.SystemTools {
@@ -13,16 +14,19 @@ namespace Mapping_Tools.Classes.SystemTools {
         public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
-        /// 
+        /// Sets the property and executes an action and invokes the property changed event if the property changed.
+        /// The action gets executed before the property changed event.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="target"></param>
-        /// <param name="value"></param>
-        /// <param name="propertyName"></param>
+        /// <param name="target">The variable to change.</param>
+        /// <param name="value">The new value.</param>
+        /// <param name="propertyName">The property name for the property changed event.</param>
+        /// <param name="action">The action to execute before the property change.</param>
         /// <returns>Whether the property changed or not.</returns>
-        protected bool Set<T>(ref T target, T value, [CallerMemberName] string propertyName = "") {
+        protected bool Set<T>(ref T target, T value, [CallerMemberName] string propertyName = "", Action action = null) {
             if (target != null && target.Equals(value)) return false;
             target = value;
+            action?.Invoke();
             RaisePropertyChanged(propertyName);
             return true;
         }
