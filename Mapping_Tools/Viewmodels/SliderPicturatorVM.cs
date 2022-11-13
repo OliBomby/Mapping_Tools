4d6e3b82aa5403f22ea85af95aa74509c22d50b2
Jonathan Keegan <jonathan.keegan@intellibus.com>
2022-11-12 19:27:14 -0500

    Add Picturator

---
+++ Mapping_Tools/Viewmodels/SliderPicturatorVM.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using Mapping_Tools.Classes;
+using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.Tools.SlideratorStuff;
+using Newtonsoft.Json;
+
+namespace Mapping_Tools.Viewmodels
+{
+
+    public class SliderPicturatorVM : BindableBase
+    {
+        #region Properties
+
+        private CancellationTokenSource previewTokenSource;
+        private readonly object previewTokenLock = new();
+
+        private bool _isProcessingPreview;
+        [JsonIgnore]
+        public bool IsProcessingPreview
+        {
+            get => _isProcessingPreview;
+            set => Set(ref _isProcessingPreview, value);
+        }
+
+        [JsonIgnore]
+        public string[] Paths { get; set; }
+
+        [JsonIgnore]
+        public bool Quick { get; set; }
+
+        private long _viewportSize;
+        public long ViewportSize
+        {
+            get => _viewportSize;
+            set => Set(ref _viewportSize, value);
+        }
+
+        [JsonIgnore]
+        public IEnumerable<long> ViewportSizes => new List<long> { 16384, 32768 };
+
+        private double _yResolution;
+        public double YResolution
+        {
+            get => _yResolution;
+            set => Set(ref _yResolution, value);
+        }
+
+        private double _sliderStartX;
+        public double SliderStartX
+        {
+            get => _sliderStartX;
+            set => Set(ref _sliderStartX, value);
+        }
+
+        private double _sliderStartY;
+        public double SliderStartY
+        {
+            get => _sliderStartY;
+            set => Set(ref _sliderStartY, value);
+        }
+
+        private double _imageStartX;
+        public double ImageStartX
+        {
+            get => _imageStartX;
+            set => Set(ref _imageStartX, value);
+        }
+
+        private double _imageStartY;
+        public double ImageStartY
+        {
+            get => _imageStartY;
+            set => Set(ref _imageStartY, value);
+        }
+
+        [JsonIgnore]
+        public IEnumerable<Color> AvailableColors {
+            get
+            {
+                var beatmap = new BeatmapEditor(IOHelper.GetCurrentBeatmapOrCurrentBeatmap()).Beatmap;
+                var comboColors = beatmap.ComboColours;
+                if (comboColors.Count() == 0) {
+                    comboColors = ComboColour.GetDefaultComboColours().ToList();
+                }
+                var availableColors = comboColors.Select(comboColor => Color.FromArgb(comboColor.Color.R, comboColor.Color.G, comboColor.Color.B));
+                if (beatmap.SpecialColours.ContainsKey("SliderTrackOverride")) {
+                    var tempColor = beatmap.SpecialColours["SliderTrackOverride"].Color;
+                    availableColors.Append(Color.FromArgb(tempColor.R, tempColor.G, tempColor.B));
+                }
+                return availableColors;
+            }
+        }
+
+
+        private bool _useMapComboColors;
+
+        public bool UseMapComboColors
+        {
+            get => _useMapComboColors;
+            set
+            {
+                if (Set(ref _useMapComboColors, value)) {
+                    if (value) {
+                        CurrentTrackColor = ComboColor;
+                    } else {
+                        CurrentTrackColor = Color.FromArgb(TrackColorPickerColor.R, TrackColorPickerColor.G, TrackColorPickerColor.B); ;
+                    }
+                    RaisePropertyChanged(nameof(ShouldShowCCPicker));
+                    RaisePropertyChanged(nameof(ShouldShowPalette));
+                }
+            }
+        }
+
+        private Color _currentTrackColor;
+        public Color CurrentTrackColor
+        {
+            get => _currentTrackColor;
+            set
+            {
+                if (Set(ref _currentTrackColor, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private Color _comboColor;
+        public Color ComboColor
+        {
+            get => _comboColor;
+            set
+            {
+                if (Set(ref _comboColor, value) && UseMapComboColors) {
+                    CurrentTrackColor = value;
+                    RaisePropertyChanged(nameof(PickedComboColor));
+                }
+            }
+        }
+
+        public String PickedComboColor
+        {
+            get => ColorTranslator.ToHtml(ComboColor);
+        }
+
+        private System.Windows.Media.Color _trackColorPickerColor;
+
+        public System.Windows.Media.Color TrackColorPickerColor
+        {
+            get => _trackColorPickerColor;
+            set {
+                if(Set(ref _trackColorPickerColor, value) && !UseMapComboColors) {
+                    CurrentTrackColor = Color.FromArgb(value.R, value.G, value.B);
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private System.Windows.Media.Color _borderColor;
+
+        public System.Windows.Media.Color BorderColor
+        {
+            get => _borderColor;
+            set
+            {
+                if (Set(ref _borderColor, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        public Visibility ShouldShowCCPicker
+        {
+            get => UseMapComboColors ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public Visibility ShouldShowPalette
+        {
+            get => UseMapComboColors ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private int _timeCode;
+        public int TimeCode
+        {
+            get => _timeCode;
+            set => Set(ref _timeCode, value);
+        }
+
+        private double _duration;
+        public double Duration
+        {
+            get => _duration;
+            set => Set(ref _duration, value);
+        }
+
+        private ICommand _uploadFile;
+        public ICommand UploadFile
+        {
+            get
+            {
+                return _uploadFile ?? (_uploadFile = new CommandHandler(() => SetFile(), () => true));
+            }
+        }
+
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        private Bitmap _bm;
+        public Bitmap BM
+        {
+            get => _bm;
+            set => Set(ref _bm, value);
+        }
+
+        private InteropBitmap _bmImage;
+
+        public InteropBitmap BMImage
+        {
+            get => _bmImage;
+            set => Set(ref _bmImage, value);
+        }
+
+        private string _pictureFile;
+        public string PictureFile
+        {
+            get => _pictureFile;
+            set {
+                Set(ref _pictureFile, value);
+                BM = new Bitmap(value);
+                RaisePropertyChanged(nameof(BM));
+                RegeneratePreview();
+            }
+        }
+
+        private void SetFile()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".png"; // Required file extension 
+            fileDialog.Filter = @"All Image Files|*.BMP;*.bmp;*.JPG;*.JPEG*.jpg;*.jpeg;*.PNG;*.png;*.GIF;*.gif;*.tif;*.tiff;*.ico;*.ICO|PNG|*.PNG;*.png|JPEG|*.JPG;*.JPEG*.jpg;*.jpeg|Bitmap(.BMP,.bmp)|*.BMP;*.bmp|GIF|*.GIF;*.gif|TIF|*.tif;*.tiff|ICO|*.ico;*.ICO";// Optional file extensions
+
+            if (fileDialog.ShowDialog() == DialogResult.OK) {
+                PictureFile = fileDialog.FileName;
+            }
+        }
+
+        private bool _blackOn;
+        public bool BlackOn
+        {
+            get => _blackOn;
+            set {
+                if (Set(ref _blackOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private bool _borderOn;
+        public bool BorderOn
+        {
+            get => _borderOn;
+            set {
+                if (Set(ref _borderOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private bool _redOn;
+        public bool RedOn
+        {
+            get => _redOn;
+            set {
+                if (Set(ref _redOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private bool _greenOn;
+        public bool GreenOn
+        {
+            get => _greenOn;
+            set {
+                if (Set(ref _greenOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private bool _blueOn;
+        public bool BlueOn
+        {
+            get => _blueOn;
+            set {
+                if (Set(ref _blueOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        private bool _alphaOn;
+        public bool AlphaOn
+        {
+            get => _alphaOn;
+            set {
+                if (Set(ref _alphaOn, value)) {
+                    RegeneratePreview();
+                }
+            }
+        }
+
+        #endregion
+
+        public void RegeneratePreview() {
+            if (BM == null) {
+                return;
+            }
+            CancellationToken ct;
+            lock (previewTokenLock) {
+                if (previewTokenSource is not null) {
+                    previewTokenSource.Cancel();
+                    previewTokenSource.Dispose();
+                }
+                previewTokenSource = new CancellationTokenSource();
+                ct = previewTokenSource.Token;
+            }
+
+            // Raise property changed for the load indicator in the preview
+            IsProcessingPreview = true;
+            var beatmap = new BeatmapEditor(IOHelper.GetCurrentBeatmapOrCurrentBeatmap()).Beatmap;
+            Bitmap bm = (Bitmap)BM.Clone();
+            Color ctc = Color.FromArgb(CurrentTrackColor.ToArgb());
+            Color bc = Color.FromArgb(BorderColor.R, BorderColor.G, BorderColor.B);
+            Task.Run(() => {
+                Bitmap newBM = SliderPicturator.Recolor(bm, ctc, bc, Color.FromArgb(0, 0, 0), !BlackOn, !BorderOn, !AlphaOn, RedOn, GreenOn, BlueOn);
+                // Send the new preview to the main thread
+                System.Windows.Application.Current.Dispatcher.Invoke(() => {
+                    IntPtr hBitmap = newBM.GetHbitmap();
+                    InteropBitmap retval;
+
+                    try {
+                        retval = (InteropBitmap)Imaging.CreateBitmapSourceFromHBitmap(
+                            hBitmap,
+                            IntPtr.Zero,
+                            Int32Rect.Empty,
+                            BitmapSizeOptions.FromEmptyOptions());
+                    }
+                    finally {
+                        DeleteObject(hBitmap);
+                    }
+                    BMImage = retval;
+                    RaisePropertyChanged(nameof(BMImage));
+                });
+
+            }).ContinueWith(task => {
+                // Show the error if one occured while generating preview
+                if (task.IsFaulted) {
+                    task.Exception.Show();
+                }
+                // Stop the processing indicator
+                System.Windows.Application.Current.Dispatcher.Invoke(() => {
+                    IsProcessingPreview = false;
+                });
+            }, ct);
+        }
+
+        private void HandleCurrentBeatmapUpdate(object sender, string currentBeatmaps)
+        {
+            RaisePropertyChanged(nameof(AvailableColors));
+        }
+
+        public SliderPicturatorVM()
+        {
+            MainWindow.AppWindow.OnUpdateCurrentBeatmap += HandleCurrentBeatmapUpdate;
+            ViewportSize = 16384;
+            TimeCode = 0;
+            Duration = 1;
+            YResolution = 1080;
+            SliderStartX = 256;
+            SliderStartY = 192;
+            ImageStartX = 0;
+            ImageStartY = 0;
+            BlackOn = true;
+            BorderOn = true;
+            RedOn = true;
+            GreenOn = true;
+            BlueOn = true;
+            AlphaOn = true;
+            UseMapComboColors = false;
+            ComboColor = Color.FromArgb(0, 0, 0);
+            TrackColorPickerColor = System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            BorderColor = System.Windows.Media.Color.FromArgb(255, 255, 255, 255);
+            BMImage = null;
+            BM = null;
+        }
+    }
+    
+}
---
+++ Mapping_Tools/Views/SliderPicturator/SliderPicturatorView.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Mapping_Tools.Classes;
+using Mapping_Tools.Classes.BeatmapHelper;
+using Mapping_Tools.Classes.BeatmapHelper.Enums;
+using Mapping_Tools.Classes.MathUtil;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.SystemTools.QuickRun;
+using Mapping_Tools.Classes.ToolHelpers;
+using Mapping_Tools.Classes.Tools.SlideratorStuff;
+using Mapping_Tools.Classes.Tools;
+using Mapping_Tools.Viewmodels;
+using System.Runtime.InteropServices;
+
+namespace Mapping_Tools.Views.SliderPicturator {
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    [SmartQuickRunUsage(SmartQuickRunTargets.AnySelection)]
+    [VerticalContentScroll]
+    [HorizontalContentScroll]
+    public partial class SliderPicturatorView : IQuickRun, ISavable<SliderPicturatorVM> {
+        public event EventHandler RunFinished;
+
+        public static readonly string ToolName = "Slider Picturator";
+
+        public static readonly string ToolDescription = $@"Import an image and this program will distort a slider into it!";
+
+        /// <inheritdoc />
+        public SliderPicturatorView()
+        {
+            InitializeComponent();
+            Width = MainWindow.AppWindow.content_views.Width;
+            Height = MainWindow.AppWindow.content_views.Height;
+            DataContext = new SliderPicturatorVM();
+            ProjectManager.LoadProject(this, message: false);
+        }
+
+        public SliderPicturatorVM ViewModel => (SliderPicturatorVM) DataContext;
+
+        protected override void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e) {
+            var bgw = sender as BackgroundWorker;
+            e.Result = Picturate((SliderPicturatorVM) e.Argument, bgw, e);
+        }
+
+       
+        private void Start_Click(object sender, RoutedEventArgs e) {
+            RunTool(new[] {IOHelper.GetCurrentBeatmapOrCurrentBeatmap()});
+        }
+
+        public void QuickRun() {
+            RunTool(new[] { IOHelper.GetCurrentBeatmapOrCurrentBeatmap() }, quick: true);
+        }
+
+        private void RunTool(string[] paths, bool quick = false) {
+            if (!CanRun) return;
+
+            // Remove logical focus to trigger LostFocus on any fields that didn't yet update the ViewModel
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(this), null);
+
+            BackupManager.SaveMapBackup(paths);
+
+            ViewModel.Paths = paths;
+            ViewModel.Quick = quick;
+
+            BackgroundWorker.RunWorkerAsync(ViewModel);
+            CanRun = false;
+        }
+
+        private string Picturate(SliderPicturatorVM arg, BackgroundWorker worker, DoWorkEventArgs _) {
+            int slidersCompleted = 0;
+
+            var reader = EditorReaderStuff.GetFullEditorReaderOrNot(out var editorReaderException1);
+
+            if (arg.PictureFile == null) {
+                throw new Exception("No image file selected.", editorReaderException1);
+            }
+
+            Bitmap img;
+            try {
+                img = new Bitmap(arg.PictureFile);
+            } catch {
+                throw new Exception("Not a valid image file.");
+            }
+
+            BeatmapEditor editor = new BeatmapEditor(IOHelper.GetCurrentBeatmapOrCurrentBeatmap());
+
+            var beatmap = editor.Beatmap;
+            long GPU = arg.ViewportSize;
+            bool R = arg.RedOn;
+            bool G = arg.GreenOn;
+            bool B = arg.BlueOn;
+            bool borderOff = !arg.BorderOn;
+            bool blackOff = !arg.BlackOn;
+            double startTime = arg.TimeCode;
+            double startPosX = arg.SliderStartX;
+            double startPosY = arg.SliderStartY;
+            double startPosPicX = arg.ImageStartX;
+            double startPosPicY = arg.ImageStartY;
+            double duration = arg.Duration;
+            double resY = arg.YResolution;
+            Vector2 startPos = new Vector2(startPosX, startPosY);
+            Vector2 startPosPic = new Vector2(startPosPicX, startPosPicY);
+
+            var circleSize = beatmap.Difficulty["CircleSize"].DoubleValue;
+            Color sliderColor = arg.CurrentTrackColor;
+            Color borderColor = Color.FromArgb(arg.BorderColor.R, arg.BorderColor.G, arg.BorderColor.B);
+            Color backgroundColor = Color.FromArgb(0,0,0);
+
+            //string[] files = Directory.GetFiles(@"C:\Users\User\Desktop\badapple\images-6fps@360p", "*.png");
+            //double TIME_SPACING = 1000/6;
+            //double time = 0;
+            //HitObject ho;
+            //List<Vector2> sliderPath;
+            //foreach (string file in files) {
+            //    img = new Bitmap(file);
+            //    sliderPath = SliderPicturator.Picturate(img, sliderColor, sliderBorder, backgroundColor, circleSize, resY, GPU, false, true);
+            //    ho = new HitObject(time, 0, SampleSet.None, SampleSet.None)
+            //    {
+            //        IsCircle = false,
+            //        IsSpinner = false,
+            //        IsHoldNote = false,
+            //        IsSlider = true
+            //    };
+            //    ho.SetAllCurvePoints(sliderPath);
+            //    ho.SliderType = PathType.Linear;
+            //    ho.PixelLength = OsuStableDistance(sliderPath);
+            //    beatmap.HitObjects.Add(ho);
+            //    time += TIME_SPACING;
+            //    worker.ReportProgress((int)Math.Round(100 * (time / TIME_SPACING) / files.Length));
+            //}
+
+            List<Vector2> sliderPath = Classes.Tools.SlideratorStuff.SliderPicturator.Picturate(img, sliderColor, borderColor, backgroundColor, circleSize, startPos, startPosPic, resY, GPU, blackOff, borderOff, R, G, B);
+
+            // Find nearest hitobject before startTime and get its combo color index
+            int currentColorIdx = 0;
+            int idx = beatmap.HitObjects.Select((hitObject) => hitObject.Time).ToList().BinarySearch(startTime);
+            if (idx < 0) {
+                idx = ~idx - 1;
+            }
+            if (idx >= 0) {
+                currentColorIdx = beatmap.HitObjects[idx].ColourIndex;
+            }
+
+            // Get requested color's combo color index, if it exists
+            int foundColorIdx = beatmap.ComboColours.FindIndex(cc => cc.Color.R == sliderColor.R && cc.Color.G == sliderColor.G && cc.Color.B == sliderColor.B);
+            if (foundColorIdx == -1) {
+                foundColorIdx = 0;
+            }
+
+            var ho = new HitObject(startTime, 0, SampleSet.None, SampleSet.None)
+            {
+                IsCircle = false,
+                IsSpinner = false,
+                IsHoldNote = false,
+                IsSlider = true,
+                ComboSkip = foundColorIdx - currentColorIdx - 1
+            };
+            ho.SetAllCurvePoints(sliderPath);
+            ho.SliderType = PathType.Linear;
+            ho.PixelLength = OsuStableDistance(sliderPath);
+            beatmap.HitObjects.Add(ho);
+            beatmap.SortHitObjects();
+
+            var timing = beatmap.BeatmapTiming;
+            var tpAfter = timing.GetRedlineAtTime(ho.Time).Copy();
+            var tpOn = tpAfter.Copy();
+
+            tpAfter.Offset = ho.Time;
+            tpOn.Offset = ho.Time - 1;  // This one will be on the slider
+
+            tpAfter.OmitFirstBarLine = true;
+            tpOn.OmitFirstBarLine = true;
+
+            // Express velocity in BPM
+            // We want ho.PixelLength = 5/3*timing.SliderMultiplier*bpm*duration/1000 so bpm = ho.PixelLength*600/(timing.SliderMultiplier*duration). Converting to MpB we get 60000*(timing.SliderMultiplier*duration/(ho.PixelLength*600)) = 100*timing.SliderMultiplier*duration/ho.PixelLength
+            tpOn.MpB = 100 * timing.SliderMultiplier * duration / ho.PixelLength;
+            // NaN SV results in removal of slider ticks
+            ho.SliderVelocity = double.NaN;
+
+            // Add redlines
+            var timingPointsChanges = new List<TimingPointsChange>();
+            timingPointsChanges.Add(new TimingPointsChange(tpOn, mpb: true, unInherited: true, omitFirstBarLine: true, fuzzyness: Precision.DOUBLE_EPSILON));
+            timingPointsChanges.Add(new TimingPointsChange(tpAfter, mpb: true, unInherited: true, omitFirstBarLine: true, fuzzyness: Precision.DOUBLE_EPSILON));
+
+            ho.Time -= 1;
+
+            timingPointsChanges.AddRange(beatmap.HitObjects.Select(bmho => {
+                var sv = bmho == ho ? bmho.SliderVelocity : timing.GetSvAtTime(bmho.Time);
+                var tp = timing.GetTimingPointAtTime(bmho.Time).Copy();
+                tp.MpB = sv;
+                tp.Offset = bmho.Time;
+                return new TimingPointsChange(tp, mpb: true, fuzzyness: Precision.DOUBLE_EPSILON);
+            }));
+
+            TimingPointsChange.ApplyChanges(timing, timingPointsChanges);
+
+            editor.SaveFile();
+
+            // Complete progressbar
+            if (worker != null && worker.WorkerReportsProgress)
+            {
+                worker.ReportProgress(100);
+            }
+
+            // Do stuff
+            RunFinished?.Invoke(this, new RunToolCompletedEventArgs(true, reader != null, arg.Quick));
+            return arg.Quick ? "" : "Done!";
+        }
+        public SliderPicturatorVM GetSaveData() {
+            return ViewModel;
+        }
+
+        public void SetSaveData(SliderPicturatorVM saveData) {
+            DataContext = saveData;
+        }
+
+        public string AutoSavePath => Path.Combine(MainWindow.AppDataPath, "sliderPicturatorproject.json");
+
+        public string DefaultSaveFolder => Path.Combine(MainWindow.AppDataPath, "Slider Picturator Projects");
+
+        private Color getOpaqueColor(Color top, Color bottom)
+        {
+            // Bottom color is assumed to be opaque
+            double GAMMA = 1;
+            return Color.FromArgb(255,
+                (byte)Math.Round(Math.Pow(Math.Pow(bottom.R, GAMMA) * (1 - top.A) + Math.Pow(top.R, GAMMA) * top.A, 1 / GAMMA)),
+                (byte)Math.Round(Math.Pow(Math.Pow(bottom.G, GAMMA) * (1 - top.A) + Math.Pow(top.G, GAMMA) * top.A, 1 / GAMMA)),
+                (byte)Math.Round(Math.Pow(Math.Pow(bottom.B, GAMMA) * (1 - top.A) + Math.Pow(top.B, GAMMA) * top.A, 1 / GAMMA)));
+        }
+        private static void MySaveBMP(byte[] buffer, int width, int height, String loc)
+        {
+            Bitmap b = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            Rectangle BoundsRect = new Rectangle(0, 0, width, height);
+            BitmapData bmpData = b.LockBits(BoundsRect,
+                                            ImageLockMode.WriteOnly,
+                                            b.PixelFormat);
+
+            IntPtr ptr = bmpData.Scan0;
+
+            // add back dummy bytes between lines, make each line be a multiple of 4 bytes
+            int skipByte = bmpData.Stride - width * 3;
+            byte[] newBuff = new byte[buffer.Length + skipByte * height];
+            for (int j = 0; j < height; j++) {
+                Buffer.BlockCopy(buffer, j * width * 3, newBuff, j * (width * 3 + skipByte), width * 3);
+            }
+
+            // fill in rgbValues
+            Marshal.Copy(newBuff, 0, ptr, newBuff.Length);
+            b.UnlockBits(bmpData);
+            b.Save(loc, ImageFormat.Bmp);
+        }
+
+        private static double OsuStableDistance(IEnumerable<Vector2> controlPoints)
+        {
+            double length = 0;
+            Vector2 cp, lp;
+            float num1, num2, num3, cpX, cpY, lpX, lpY;
+            for (int i = 1; i < controlPoints.Count(); i++) {
+                lp = controlPoints.ElementAt(i - 1);
+                cp = controlPoints.ElementAt(i);
+                num1 = (float)Math.Round(lp.X) - (float)Math.Round(cp.X);
+                num2 = (float)Math.Round(lp.Y) - (float)Math.Round(cp.Y);
+                num3 = num1 * num1 + num2 * num2;
+
+                length += (float)Math.Sqrt((double)num3);
+            }
+            return length;
+        }
+    }
+}
