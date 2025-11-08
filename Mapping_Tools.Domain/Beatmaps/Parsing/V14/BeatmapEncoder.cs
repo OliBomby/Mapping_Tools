c485430f0e996a1e801f8e8563cde6e8ecbf6566
OliBomby <olivierschipper@gmail.com>
2025-11-08 13:19:23 +0100

    dont encode enumerable

--- Mapping_Tools.Domain/Beatmaps/Parsing/V14/BeatmapEncoder.cs
+++ Mapping_Tools.Domain/Beatmaps/Parsing/V14/BeatmapEncoder.cs
@@ -11,111 +11,113 @@ public class BeatmapEncoder(
     IEncoder<HitObject> hitObjectEncoder,
     IEncoder<TimingPoint> timingPointEncoder)
     : IEncoder<Beatmap> {
-    public BeatmapEncoder() : this(new StoryboardEncoder(), new HitObjectEncoder(), new TimingPointEncoder()) { }
-
-    private IEnumerable<string> EncodeEnumerable(Beatmap beatmap) {
-        // Getting all the stuff
-        yield return "osu file format v" + beatmap.BeatmapVersion.ToInvariant();
-        yield return "";
-
-        yield return "[General]";
-        yield return "AudioFilename: " + beatmap.General.AudioFilename;
-        yield return "AudioLeadIn: " + beatmap.General.AudioLeadIn.ToInvariant();
-        //yield return "AudioHash: " + AudioEngine.AudioMd5;
-        yield return "PreviewTime: " + beatmap.General.PreviewTime.ToInvariant();
-        yield return "Countdown: " + ((int)beatmap.General.Countdown).ToInvariant();
-        yield return "SampleSet: " + beatmap.General.SampleSet;
-        yield return "StackLeniency: " + beatmap.General.StackLeniency.ToInvariant();
-        yield return "Mode: " + ((int)beatmap.General.Mode).ToInvariant();
-        yield return "LetterboxInBreaks: " + (beatmap.General.LetterboxInBreaks ? "1" : "0");
-        if (!beatmap.General.StoryFireInFront)
-            yield return "StoryFireInFront: 0";
-        if (beatmap.General.UseSkinSprites)
-            yield return "UseSkinSprites: 1";
-        if (beatmap.General.AlwaysShowPlayfield)
-            yield return "AlwaysShowPlayfield: 1";
-        if (beatmap.General.OverlayPosition != OverlayPosition.NoChange)
-            yield return "OverlayPosition: " + beatmap.General.OverlayPosition;
-        if (!string.IsNullOrEmpty(beatmap.General.SkinPreference))
-            yield return "SkinPreference:" + beatmap.General.SkinPreference;
-        if (beatmap.General.EpilepsyWarning)
-            yield return "EpilepsyWarning: 1";
-        if (beatmap.General.CountdownOffset > 0)
-            yield return "CountdownOffset: " + beatmap.General.CountdownOffset.ToInvariant();
-        if (beatmap.General.Mode == GameMode.Mania)
-            yield return "SpecialStyle: " + (beatmap.General.SpecialStyle ? "1" : "0");
-        if (beatmap.BeatmapVersion > 10 || beatmap.General.WidescreenStoryboard)
-            yield return "WidescreenStoryboard: " + (beatmap.General.WidescreenStoryboard ? "1" : "0");
-        if (beatmap.General.SamplesMatchPlaybackRate)
-            yield return "SamplesMatchPlaybackRate: 1";
-        yield return "";
-
-        yield return "[Editor]";
-
-        if (beatmap.Editor.Bookmarks.Count > 0) {
-            var builder = new StringBuilder();
-            builder.AppendJoin(',', beatmap.Editor.Bookmarks.Select(b => b.ToRoundInvariant()));
-            yield return "Bookmarks: " + builder;
-        }
-
-        yield return "DistanceSpacing: " + beatmap.Editor.DistanceSpacing.ToInvariant();
-        yield return "BeatDivisor: " + beatmap.Editor.BeatDivisor.ToInvariant();
-        yield return "GridSize: " + beatmap.Editor.GridSize.ToInvariant();
-        if (beatmap.BeatmapVersion > 10 || beatmap.Editor.TimelineZoom != 1f)
-            yield return "TimelineZoom: " + beatmap.Editor.TimelineZoom.ToInvariant();
-        yield return "";
-
-        yield return "[Metadata]";
-        yield return "Title:" + beatmap.Metadata.Title;
-        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.TitleUnicode != beatmap.Metadata.Title)
-            yield return "TitleUnicode:" + beatmap.Metadata.TitleUnicode;
-        yield return "Artist:" + beatmap.Metadata.Artist;
-        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.ArtistUnicode != beatmap.Metadata.Artist)
-            yield return "ArtistUnicode:" + beatmap.Metadata.ArtistUnicode;
-        yield return "Creator:" + beatmap.Metadata.Creator;
-        yield return "Version:" + beatmap.Metadata.Version;
-        yield return "Source:" + beatmap.Metadata.Source;
-        yield return "Tags:" + beatmap.Metadata.Tags;
-        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.BeatmapId != 0)
-            yield return "BeatmapID:" + beatmap.Metadata.BeatmapId.ToInvariant();
-        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.BeatmapSetId != -1)
-            yield return "BeatmapSetID:" + beatmap.Metadata.BeatmapSetId.ToInvariant();
-        yield return "";
-
-        yield return "[Difficulty]";
-        yield return "HPDrainRate:" + beatmap.Difficulty.HpDrainRate.ToInvariant();
-        yield return "CircleSize:" + beatmap.Difficulty.CircleSize.ToInvariant();
-        yield return "OverallDifficulty:" + beatmap.Difficulty.OverallDifficulty.ToInvariant();
-        yield return "ApproachRate:" + beatmap.Difficulty.ApproachRate.ToInvariant();
-        yield return "SliderMultiplier:" + beatmap.Difficulty.SliderMultiplier.ToInvariant();
-        yield return "SliderTickRate:" + beatmap.Difficulty.SliderTickRate.ToInvariant();
-        yield return "";
-
-        yield return storyboardEncoder.Encode(beatmap.Storyboard);
-        yield return "[TimingPoints]";
-        foreach (TimingPoint tp in beatmap.BeatmapTiming.TimingPoints) {
-            yield return timingPointEncoder.Encode(tp);
-        }
-        yield return "";
-        if (beatmap.ComboColoursList.Any()) {
-            yield return "";
-            yield return "[Colours]";
-            foreach (string s in beatmap.ComboColoursList.Select((comboColour, i) =>
-                         $"Combo{i + 1} : {ComboColour.SerializeComboColour(comboColour)}"))
-                yield return s;
-            foreach (string s in beatmap.SpecialColours.Select(specialColour => specialColour.Key + " : " +
-                                                                                ComboColour.SerializeComboColour(specialColour.Value)))
-                yield return s;
-        }
-        yield return "";
-        yield return "[HitObjects]";
-        foreach (HitObject ho in beatmap.HitObjects) {
-            yield return hitObjectEncoder.Encode(ho);
-        }
-        yield return "";
+    public BeatmapEncoder() : this(new StoryboardEncoder(), new HitObjectEncoder(), new TimingPointEncoder()) {
     }
 
-    public string Encode(Beatmap obj) {
-        return string.Join(Environment.NewLine, EncodeEnumerable(obj));
+    public string Encode(Beatmap beatmap) {
+        var builder = new StringBuilder();
+
+        builder.AppendLine("osu file format v" + beatmap.BeatmapVersion.ToInvariant());
+        builder.AppendLine();
+
+        builder.AppendLine("[General]");
+        builder.AppendLine("AudioFilename: " + beatmap.General.AudioFilename);
+        builder.AppendLine("AudioLeadIn: " + beatmap.General.AudioLeadIn.ToInvariant());
+        //builder.AppendLine("AudioHash: " + AudioEngine.AudioMd5);
+        builder.AppendLine("PreviewTime: " + beatmap.General.PreviewTime.ToInvariant());
+        builder.AppendLine("Countdown: " + ((int) beatmap.General.Countdown).ToInvariant());
+        builder.AppendLine("SampleSet: " + beatmap.General.SampleSet);
+        builder.AppendLine("StackLeniency: " + beatmap.General.StackLeniency.ToInvariant());
+        builder.AppendLine("Mode: " + ((int) beatmap.General.Mode).ToInvariant());
+        builder.AppendLine("LetterboxInBreaks: " + (beatmap.General.LetterboxInBreaks ? "1" : "0"));
+        if (!beatmap.General.StoryFireInFront)
+            builder.AppendLine("StoryFireInFront: 0");
+        if (beatmap.General.UseSkinSprites)
+            builder.AppendLine("UseSkinSprites: 1");
+        if (beatmap.General.AlwaysShowPlayfield)
+            builder.AppendLine("AlwaysShowPlayfield: 1");
+        if (beatmap.General.OverlayPosition != OverlayPosition.NoChange)
+            builder.AppendLine("OverlayPosition: " + beatmap.General.OverlayPosition);
+        if (!string.IsNullOrEmpty(beatmap.General.SkinPreference))
+            builder.AppendLine("SkinPreference:" + beatmap.General.SkinPreference);
+        if (beatmap.General.EpilepsyWarning)
+            builder.AppendLine("EpilepsyWarning: 1");
+        if (beatmap.General.CountdownOffset > 0)
+            builder.AppendLine("CountdownOffset: " + beatmap.General.CountdownOffset.ToInvariant());
+        if (beatmap.General.Mode == GameMode.Mania)
+            builder.AppendLine("SpecialStyle: " + (beatmap.General.SpecialStyle ? "1" : "0"));
+        if (beatmap.BeatmapVersion > 10 || beatmap.General.WidescreenStoryboard)
+            builder.AppendLine("WidescreenStoryboard: " + (beatmap.General.WidescreenStoryboard ? "1" : "0"));
+        if (beatmap.General.SamplesMatchPlaybackRate)
+            builder.AppendLine("SamplesMatchPlaybackRate: 1");
+        builder.AppendLine();
+
+        builder.AppendLine("[Editor]");
+
+        if (beatmap.Editor.Bookmarks.Count > 0) {
+            var bookmarksBuilder = new StringBuilder();
+            bookmarksBuilder.AppendJoin(',', beatmap.Editor.Bookmarks.Select(b => b.ToRoundInvariant()));
+            builder.AppendLine("Bookmarks: " + bookmarksBuilder);
+        }
+
+        builder.AppendLine("DistanceSpacing: " + beatmap.Editor.DistanceSpacing.ToInvariant());
+        builder.AppendLine("BeatDivisor: " + beatmap.Editor.BeatDivisor.ToInvariant());
+        builder.AppendLine("GridSize: " + beatmap.Editor.GridSize.ToInvariant());
+        if (beatmap.BeatmapVersion > 10 || beatmap.Editor.TimelineZoom != 1f)
+            builder.AppendLine("TimelineZoom: " + beatmap.Editor.TimelineZoom.ToInvariant());
+        builder.AppendLine();
+
+        builder.AppendLine("[Metadata]");
+        builder.AppendLine("Title:" + beatmap.Metadata.Title);
+        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.TitleUnicode != beatmap.Metadata.Title)
+            builder.AppendLine("TitleUnicode:" + beatmap.Metadata.TitleUnicode);
+        builder.AppendLine("Artist:" + beatmap.Metadata.Artist);
+        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.ArtistUnicode != beatmap.Metadata.Artist)
+            builder.AppendLine("ArtistUnicode:" + beatmap.Metadata.ArtistUnicode);
+        builder.AppendLine("Creator:" + beatmap.Metadata.Creator);
+        builder.AppendLine("Version:" + beatmap.Metadata.Version);
+        builder.AppendLine("Source:" + beatmap.Metadata.Source);
+        builder.AppendLine("Tags:" + beatmap.Metadata.Tags);
+        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.BeatmapId != 0)
+            builder.AppendLine("BeatmapID:" + beatmap.Metadata.BeatmapId.ToInvariant());
+        if (beatmap.BeatmapVersion > 9 || beatmap.Metadata.BeatmapSetId != -1)
+            builder.AppendLine("BeatmapSetID:" + beatmap.Metadata.BeatmapSetId.ToInvariant());
+        builder.AppendLine();
+
+        builder.AppendLine("[Difficulty]");
+        builder.AppendLine("HPDrainRate:" + beatmap.Difficulty.HpDrainRate.ToInvariant());
+        builder.AppendLine("CircleSize:" + beatmap.Difficulty.CircleSize.ToInvariant());
+        builder.AppendLine("OverallDifficulty:" + beatmap.Difficulty.OverallDifficulty.ToInvariant());
+        builder.AppendLine("ApproachRate:" + beatmap.Difficulty.ApproachRate.ToInvariant());
+        builder.AppendLine("SliderMultiplier:" + beatmap.Difficulty.SliderMultiplier.ToInvariant());
+        builder.AppendLine("SliderTickRate:" + beatmap.Difficulty.SliderTickRate.ToInvariant());
+        builder.AppendLine();
+
+        builder.AppendLine(storyboardEncoder.Encode(beatmap.Storyboard));
+        builder.AppendLine("[TimingPoints]");
+        foreach (TimingPoint tp in beatmap.BeatmapTiming.TimingPoints) {
+            builder.AppendLine(timingPointEncoder.Encode(tp));
+        }
+
+        builder.AppendLine();
+        if (beatmap.ComboColoursList.Count != 0) {
+            builder.AppendLine();
+            builder.AppendLine("[Colours]");
+            foreach (string s in beatmap.ComboColoursList.Select((comboColour, i) =>
+                         $"Combo{i + 1} : {ComboColour.SerializeComboColour(comboColour)}"))
+                builder.AppendLine(s);
+            foreach (string s in beatmap.SpecialColours.Select(specialColour => specialColour.Key + " : " + ComboColour.SerializeComboColour(specialColour.Value)))
+                builder.AppendLine(s);
+        }
+
+        builder.AppendLine();
+        builder.AppendLine("[HitObjects]");
+        foreach (HitObject ho in beatmap.HitObjects) {
+            builder.AppendLine(hitObjectEncoder.Encode(ho));
+        }
+
+        builder.AppendLine();
+
+        return builder.ToString();
     }
 }
--- Mapping_Tools.Domain/Beatmaps/Parsing/V14/StoryboardEncoder.cs
+++ Mapping_Tools.Domain/Beatmaps/Parsing/V14/StoryboardEncoder.cs
@@ -3,32 +3,31 @@
 namespace Mapping_Tools.Domain.Beatmaps.Parsing.V14;
 
 public class StoryboardEncoder : IEncoder<Storyboard> {
-    private static IEnumerable<string> EncodeEnumerable(Storyboard obj) {
-        yield return "[Events]";
-        yield return "//Background and Video events";
-        foreach (string s in obj.BackgroundAndVideoEvents.Select(e => e.GetLine())) yield return s;
-        yield return "//Break Periods";
-        foreach (string s in obj.BreakPeriods.Select(b => b.GetLine())) yield return s;
-        yield return "//Storyboard Layer 0 (Background)";
-        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerBackground)) yield return s;
-        yield return "//Storyboard Layer 1 (Fail)";
-        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerFail)) yield return s;
-        yield return "//Storyboard Layer 2 (Pass)";
-        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerPass)) yield return s;
-        yield return "//Storyboard Layer 3 (Foreground)";
-        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerForeground)) yield return s;
-        yield return "//Storyboard Layer 4 (Overlay)";
-        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerOverlay)) yield return s;
-        yield return "//Storyboard Sound Samples";
-        foreach (string s in obj.StoryboardSoundSamples.Select(sbss => sbss.GetLine())) yield return s;
-        if (obj.BackgroundColourTransformations.Count > 0) {
-            yield return "//Background Colour Transformations";
-            foreach (string s in obj.BackgroundColourTransformations.Select(sbss => sbss.GetLine())) yield return s;
-        }
-        yield return "";
-    }
-
     public string Encode(Storyboard obj) {
-        return string.Join(Environment.NewLine, EncodeEnumerable(obj));
+        var builder = new System.Text.StringBuilder();
+        builder.AppendLine("[Events]");
+        builder.AppendLine("//Background and Video events");
+        foreach (string s in obj.BackgroundAndVideoEvents.Select(e => e.GetLine())) builder.AppendLine(s);
+        builder.AppendLine("//Break Periods");
+        foreach (string s in obj.BreakPeriods.Select(b => b.GetLine())) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Layer 0 (Background)");
+        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerBackground)) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Layer 1 (Fail)");
+        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerFail)) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Layer 2 (Pass)");
+        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerPass)) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Layer 3 (Foreground)");
+        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerForeground)) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Layer 4 (Overlay)");
+        foreach (string s in Event.SerializeEventTree(obj.StoryboardLayerOverlay)) builder.AppendLine(s);
+        builder.AppendLine("//Storyboard Sound Samples");
+        foreach (string s in obj.StoryboardSoundSamples.Select(sbss => sbss.GetLine())) builder.AppendLine(s);
+        if (obj.BackgroundColourTransformations.Count > 0) {
+            builder.AppendLine("//Background Colour Transformations");
+            foreach (string s in obj.BackgroundColourTransformations.Select(sbss => sbss.GetLine())) builder.AppendLine(s);
+        }
+
+        builder.AppendLine();
+        return builder.ToString();
     }
 }
