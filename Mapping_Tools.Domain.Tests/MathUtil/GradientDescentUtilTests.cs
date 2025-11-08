ba4fa566b216c207ef58ce6c52044edecb514b50
OliBomby <olivierschipper@gmail.com>
2025-11-08 20:17:08 +0100

    add mathUtil tests

---
+++ Mapping_Tools.Domain.Tests/MathUtil/GradientDescentUtilTests.cs
@@ -0,0 +1,23 @@
+﻿using Mapping_Tools.Domain.MathUtil;
+
+namespace Mapping_Tools.Domain.Tests.MathUtil;
+
+[TestFixture]
+public class GradientDescentUtilTests {
+    [Test]
+    public void GradientDescentTest() {
+        Func<double, double> func = Math.Sin;
+        const double lower = 0;
+        const double upper = 2 * Math.PI;
+        const double rate = 0.5;
+
+        double result1 = GradientDescentUtil.GradientDescent(func, lower, upper, rate);
+        Assert.That(result1, Is.EqualTo(1.5 * Math.PI).Within(0.001));
+
+        double result2 = GradientDescentUtil.GradientAscent(func, lower, upper, rate);
+        Assert.That(result2, Is.EqualTo(0.5 * Math.PI).Within(0.001));
+
+        double result3 = GradientDescentUtil.GradientDescent(func, lower + 4 * Math.PI, upper + 4 * Math.PI, rate);
+        Assert.That(result3, Is.EqualTo(1.5 * Math.PI + 4 * Math.PI).Within(0.001));
+    }
+}
---
+++ Mapping_Tools.Domain.Tests/MathUtil/Vector2Tests.cs
@@ -0,0 +1,17 @@
+﻿using Mapping_Tools.Domain.MathUtil;
+
+namespace Mapping_Tools.Domain.Tests.MathUtil;
+
+[TestFixture]
+public class Vector2Tests {
+    [Test]
+    public void Vector2AddTest() {
+        var v1 = new Vector2(1, -4);
+        var v2 = new Vector2(-8, 16);
+        var expected = new Vector2(-7, 12);
+
+        var actual = v1 + v2;
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
