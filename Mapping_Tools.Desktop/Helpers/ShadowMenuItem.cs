d7b489af5ec368c325d589f75a16c64567897ab3
OliBomby <olivierschipper@gmail.com>
2025-11-15 13:56:07 +0100

    add shadows to popups

---
+++ Mapping_Tools.Desktop/Helpers/ShadowMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace Mapping_Tools.Desktop.Helpers;
+
+
+public class ShadowMenuItem : MenuItem
+{
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        if (e.NameScope.Find("PART_Popup") is Popup popup)
+        {
+            popup.WindowManagerAddShadowHint = true;
+        }
+    }
+}
