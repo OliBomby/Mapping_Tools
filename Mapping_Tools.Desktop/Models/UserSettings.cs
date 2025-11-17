8d32a99e4465f5b2848aad6e81b64326b1998b19
Olivier Schipper <olivierschipper@gmail.com>
2025-11-17 09:06:21 +0100

    Update UserSettings.cs

--- Mapping_Tools.Desktop/Models/UserSettings.cs
+++ Mapping_Tools.Desktop/Models/UserSettings.cs
@@ -1,40 +1,119 @@
 ﻿using System;
 using System.Collections.Generic;
 using Avalonia;
+using ReactiveUI;
+using ReactiveUI.SourceGenerators;
 
 namespace Mapping_Tools.Desktop.Models;
 
-public record UserSettings(
-    List<string> RecentMaps,
-    List<string> FavoriteTools,
-    TimeSpan PeriodicBackupInterval,
-    PixelRect? MainWindowRestoreBounds = null,
-    bool MainWindowMaximized = false,
-    string OsuPath = "",
-    string SongsPath = "",
-    string BackupsPath = "",
-    string OsuConfigPath = "",
-    bool MakeBackups = true,
-    bool UseEditorReader = true,
-    bool OverrideOsuSave = false,
-    bool AutoReload = true,
-    bool AlwaysQuickRun = false,
-    string? QuickRunHotkey = null,
-    bool SmartQuickRunEnabled = true,
-    string NoneQuickRunTool = "<Current Tool>",
-    string SingleQuickRunTool = "<Current Tool>",
-    string MultipleQuickRunTool = "<Current Tool>",
-    string? BetterSaveHotkey = null,
-    int MaxBackupFiles = 1000,
-    bool MakePeriodicBackups = true,
-    bool CurrentBeatmapDefaultFolder = true,
-    string? QuickUndoHotkey = null,
-    Version? SkipVersion = null
-)
-{
-    public UserSettings() : this(
-        RecentMaps: [],
-        FavoriteTools: [],
-        PeriodicBackupInterval: TimeSpan.FromMinutes(10)
-    ) { }
+public partial class UserSettings : ReactiveObject {
+    [Reactive]
+    private List<string[]> _recentMaps;
+
+    [Reactive]
+    private List<string> _favoriteTools;
+
+    [Reactive]
+    private Rect? _mainWindowRestoreBounds;
+
+    [Reactive]
+    private bool _mainWindowMaximized;
+
+    [Reactive]
+    private string _osuPath;
+
+    [Reactive]
+    private string _songsPath;
+
+    [Reactive]
+    private string _backupsPath;
+
+    [Reactive]
+    private string _osuConfigPath;
+
+    [Reactive]
+    private bool _makeBackups;
+
+    [Reactive]
+    private bool _useEditorReader;
+
+    [Reactive]
+    private bool _overrideOsuSave;
+
+    [Reactive]
+    private bool _autoReload;
+
+    [Reactive]
+    private bool _alwaysQuickRun;
+
+    [Reactive]
+    private string? _quickRunHotkey;
+
+    [Reactive]
+    private bool _smartQuickRunEnabled;
+
+    [Reactive]
+    private string _noneQuickRunTool;
+
+    [Reactive]
+    private string _singleQuickRunTool;
+
+    [Reactive]
+    private string _multipleQuickRunTool;
+
+    [Reactive]
+    private string? _betterSaveHotkey;
+
+    [Reactive]
+    private int _maxBackupFiles;
+
+    [Reactive]
+    private bool _makePeriodicBackups;
+
+    [Reactive]
+    private TimeSpan _periodicBackupInterval;
+
+    [Reactive]
+    private bool _currentBeatmapDefaultFolder;
+
+    [Reactive]
+    private string? _quickUndoHotkey;
+    
+    [Reactive]
+    private Version? _skipVersion;
+
+    /// <summary>
+    /// Makes a new Settings objects and initializes default settings.
+    /// </summary>
+    public UserSettings() {
+        _recentMaps = [];
+        _favoriteTools = [];
+        _mainWindowRestoreBounds = null;
+        _mainWindowMaximized = false;
+        _osuPath = "";
+        _songsPath = "";
+        _backupsPath = "";
+        _osuConfigPath = "";
+        _makeBackups = true;
+        _useEditorReader = true;
+        _overrideOsuSave = false;
+        _autoReload = true;
+        _alwaysQuickRun = false;
+        _smartQuickRunEnabled = true;
+        _noneQuickRunTool = "<Current Tool>";
+        _singleQuickRunTool = "<Current Tool>";
+        _multipleQuickRunTool = "<Current Tool>";
+        _maxBackupFiles = 1000;
+        _makePeriodicBackups = true;
+        _periodicBackupInterval = TimeSpan.FromMinutes(10);
+        _currentBeatmapDefaultFolder = true;
+        _skipVersion = null;
+    }
+
+    public void CopyTo(UserSettings other) {
+        foreach (var prop in typeof(UserSettings).GetProperties()) {
+            if (!prop.CanRead || !prop.CanWrite) { continue; }
+            prop.SetValue(other, prop.GetValue(this));
+        }
+    }
 }
