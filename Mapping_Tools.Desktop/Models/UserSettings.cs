9d49b45f218edc663956dc9c77454b2e8b90a24f
Olivier Schipper <olivierschipper@gmail.com>
2025-11-17 08:57:06 +0100

    Create user settings service

---
+++ Mapping_Tools.Desktop/Models/UserSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia;
+
+namespace Mapping_Tools.Desktop.Models;
+
+public record UserSettings(
+    List<string> RecentMaps,
+    List<string> FavoriteTools,
+    TimeSpan PeriodicBackupInterval,
+    PixelRect? MainWindowRestoreBounds = null,
+    bool MainWindowMaximized = false,
+    string OsuPath = "",
+    string SongsPath = "",
+    string BackupsPath = "",
+    string OsuConfigPath = "",
+    bool MakeBackups = true,
+    bool UseEditorReader = true,
+    bool OverrideOsuSave = false,
+    bool AutoReload = true,
+    bool AlwaysQuickRun = false,
+    string? QuickRunHotkey = null,
+    bool SmartQuickRunEnabled = true,
+    string NoneQuickRunTool = "<Current Tool>",
+    string SingleQuickRunTool = "<Current Tool>",
+    string MultipleQuickRunTool = "<Current Tool>",
+    string? BetterSaveHotkey = null,
+    int MaxBackupFiles = 1000,
+    bool MakePeriodicBackups = true,
+    bool CurrentBeatmapDefaultFolder = true,
+    string? QuickUndoHotkey = null,
+    Version? SkipVersion = null
+)
+{
+    public UserSettings() : this(
+        RecentMaps: [],
+        FavoriteTools: [],
+        PeriodicBackupInterval: TimeSpan.FromMinutes(10)
+    ) { }
+}
---
+++ Mapping_Tools.Desktop/Services/UserSettingsService.cs
@@ -0,0 +1,16 @@
+﻿using Mapping_Tools.Application;
+using Mapping_Tools.Application.Persistence;
+using Mapping_Tools.Desktop.Models;
+
+namespace Mapping_Tools.Desktop.Services;
+
+public class UserSettingsService
+{
+    public UserSettings Settings { get; }
+    
+    public UserSettingsService(IStateStore stateStore, IAppLifecycle appLifecycle)
+    {
+        Settings = stateStore.LoadAsync<UserSettings>("user_settings").GetAwaiter().GetResult() ?? new UserSettings();
+        appLifecycle.UICleanup.Register(() => stateStore.SaveAsync("user_settings", Settings).GetAwaiter().GetResult());
+    }
+}
